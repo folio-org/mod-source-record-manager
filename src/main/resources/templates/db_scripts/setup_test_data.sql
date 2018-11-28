f912bbab1c51c926e0b9e575331ca891bd297c85
Taras Spashchenko <Taras_Spashchenko@epam.com>
2018-11-28 17:14:56 +0200

    MODSOURMAN-26 Stub data for job executions

---
+++ src/main/resources/templates/db_scripts/setup_test_data.sql
@@ -0,0 +1,251 @@
+INSERT INTO JOB_EXECUTIONS (_id, jsonb) values
+('5cff8cbd-ecb3-404c-8eef-fa030d3579ea', '{
+    "jobProfileName": "Authority updates",
+    "sourcePath": "import7.marc",
+    "id": "5cff8cbd-ecb3-404c-8eef-fa030d3579ea",
+    "hrId": "112984432",
+    "status": "PROCESSING_FINISHED",
+    "runBy": {
+      "firstName": "John",
+      "lastName": "Doe"
+    },
+    "progress": {
+      "current": 20000,
+      "total": 20000
+    },
+    "startedDate": "2018-11-20T23:26:44.000",
+    "completedDate": "2018-11-21T00:38:44.000"
+  }'),
+('63a30136-06cb-405b-be72-f4d001614b4f', '{
+    "jobProfileName": "Authority updates",
+    "sourcePath": "import8.marc",
+    "id": "63a30136-06cb-405b-be72-f4d001614b4f",
+    "hrId": "112974998",
+    "status": "PROCESSING_FINISHED",
+    "runBy": {
+      "firstName": "John",
+      "lastName": "Lennon"
+    },
+    "progress": {
+      "current": 1000,
+      "total": 1000
+    },
+    "startedDate": "2018-11-20T23:38:44.000",
+    "completedDate": "2018-11-20T23:50:44.000"
+  }'),
+('cc64986d-ec72-4b01-a3fd-1762f950e49a', '{
+    "jobProfileName": "Marc bib jobs",
+    "sourcePath": "import_156.mrc",
+    "id": "cc64986d-ec72-4b01-a3fd-1762f950e49a",
+    "hrId": "122984588",
+    "status": "PARSING_IN_PROGRESS",
+    "runBy": {
+      "firstName": "John",
+      "lastName": "Doe"
+    },
+    "progress": {
+      "current": 1500,
+      "total": 20000
+    },
+    "startedDate": "2018-11-20T13:26:44.000",
+    "completedDate": ""
+  }'),
+('e6219fe2-abbc-47b6-afe4-8910d49fc337', '{
+    "jobProfileName": "Marc jobs",
+    "sourcePath": "import_209.mrc",
+    "id": "e6219fe2-abbc-47b6-afe4-8910d49fc337",
+    "hrId": "122984589",
+    "status": "PARSING_IN_PROGRESS",
+    "runBy": {
+      "firstName": "Indiana",
+      "lastName": "Jones"
+    },
+    "progress": {
+      "current": 2750,
+      "total": 9000
+    },
+    "startedDate": "2018-11-20T14:14:44.000",
+    "completedDate": ""
+  }'),
+('28f53010-a9d9-4f96-8f81-04a0549554d0', '{
+    "jobProfileName": "Marc bib",
+    "sourcePath": "import_159.mrc",
+    "id": "28f53010-a9d9-4f96-8f81-04a0549554d0",
+    "hrId": "182984219",
+    "status": "PARSING_IN_PROGRESS",
+    "runBy": {
+      "firstName": "Mark",
+      "lastName": "Anderson"
+    },
+    "progress": {
+      "current": 350,
+      "total": 2000
+    },
+    "startedDate": "2018-11-20T14:56:12.000",
+    "completedDate": ""
+  }'),
+('d2b29a0c-3862-456e-9236-d14d33a4accb', '{
+    "jobProfileName": "Library indexing",
+    "sourcePath": "import_150.mrc",
+    "id": "d2b29a0c-3862-456e-9236-d14d33a4accb",
+    "hrId": "182984532",
+    "status": "PROCESSING_IN_PROGRESS",
+    "runBy": {
+      "firstName": "Jack",
+      "lastName": "Doe"
+    },
+    "progress": {
+      "current": 1750,
+      "total": 2000
+    },
+    "startedDate": "2018-11-20T14:32:44.000",
+    "completedDate": ""
+  }'),
+('469eba83-41d1-4161-bd1a-0f46d8764c6a', '{
+    "jobProfileName": "Library indexing",
+    "sourcePath": "import_102.mrc",
+    "id": "469eba83-41d1-4161-bd1a-0f46d8764c6a",
+    "hrId": "182982989",
+    "status": "PROCESSING_IN_PROGRESS",
+    "runBy": {
+      "firstName": "Marie",
+      "lastName": "Curie"
+    },
+    "progress": {
+      "current": 90,
+      "total": 2000
+    },
+    "startedDate": "2018-11-20T14:50:44.000",
+    "completedDate": ""
+  }'),
+('2f42bcfa-9ad1-492d-a2b4-280020345f64', '{
+    "jobProfileName": "Standard BIB Import",
+    "sourcePath": "importBIB017.marc",
+    "id": "2f42bcfa-9ad1-492d-a2b4-280020345f64",
+    "hrId": "222984498",
+    "status": "PROCESSING_FINISHED",
+    "runBy": {
+      "firstName": "Caleb",
+      "lastName": "Hunter"
+    },
+    "progress": {
+      "current": 24000,
+      "total": 24000
+    },
+    "startedDate": "2018-10-09T22:26:12.000",
+    "completedDate": "2018-11-21T00:01:17.000"
+  }'),
+('bf75b55e-12de-4329-ad75-879cc9ec8e34', '{
+    "jobProfileName": "BIB Import from Boston",
+    "sourcePath": "importBoston.marc",
+    "id": "bf75b55e-12de-4329-ad75-879cc9ec8e34",
+    "hrId": "143274991",
+    "status": "PROCESSING_FINISHED",
+    "runBy": {
+      "firstName": "Taylor",
+      "lastName": "Edwards"
+    },
+    "progress": {
+      "current": 1000,
+      "total": 1000
+    },
+    "startedDate": "2018-11-20T23:38:44.000",
+    "completedDate": "2018-11-20T23:50:44.000"
+  }'),
+('78d4a7de-0677-40fb-875e-588ef87628c9', '{
+    "jobProfileName": "Main bib jobs (MARC)",
+    "sourcePath": "import_332.mrc",
+    "id": "78d4a7de-0677-40fb-875e-588ef87628c9",
+    "hrId": "634984588",
+    "status": "PARSING_IN_PROGRESS",
+    "runBy": {
+      "firstName": "Oliver",
+      "lastName": "Clarke"
+    },
+    "progress": {
+      "current": 26525,
+      "total": 27324
+    },
+    "startedDate": "2018-11-20T13:26:44.000",
+    "completedDate": ""
+  }'),
+('2bec675c-f605-4622-8a59-b43385181d7e', '{
+    "jobProfileName": "Main bib jobs (MARC)",
+    "sourcePath": "import_211.mrc",
+    "id": "2bec675c-f605-4622-8a59-b43385181d7e",
+    "hrId": "126344589",
+    "status": "PARSING_IN_PROGRESS",
+    "runBy": {
+      "firstName": "Zachary",
+      "lastName": "Allen"
+    },
+    "progress": {
+      "current": 4750,
+      "total": 19523
+    },
+    "startedDate": "2018-11-20T14:14:44.000",
+    "completedDate": ""
+  }'),
+('3a560d98-10bd-4b29-a172-6cfc15a1420f', '{
+    "jobProfileName": "Custom (CH) Marc bib",
+    "sourcePath": "Supplier32_import_88.mrc",
+    "id": "3a560d98-10bd-4b29-a172-6cfc15a1420f",
+    "hrId": "837984210",
+    "status": "PARSING_IN_PROGRESS",
+    "runBy": {
+      "firstName": "Christopher",
+      "lastName": "Baxter"
+    },
+    "progress": {
+      "current": 350,
+      "total": 2432
+    },
+    "startedDate": "2018-11-20T14:44:44.000",
+    "completedDate": ""
+  }'),
+('bd4e22fd-799a-4fc3-9a2c-a17379921df7', '{
+    "jobProfileName": "Library indexing",
+    "sourcePath": "import_idx.mrc",
+    "id": "bd4e22fd-799a-4fc3-9a2c-a17379921df7",
+    "hrId": "321984532",
+    "status": "PROCESSING_IN_PROGRESS",
+    "runBy": {
+      "firstName": "Silas",
+      "lastName": "Atkinson"
+    },
+    "progress": {
+      "current": 1750,
+      "total": 2837
+    },
+    "startedDate": "2018-11-20T14:32:44.000",
+    "completedDate": ""
+  }'),
+('7ca6e90e-ec2a-444f-a442-ccb65d88699e', '{
+    "jobProfileName": "Library indexing",
+    "sourcePath": "import_idx_unq.mrc",
+    "id": "7ca6e90e-ec2a-444f-a442-ccb65d88699e",
+    "hrId": "237482989",
+    "status": "PROCESSING_IN_PROGRESS",
+    "runBy": {
+      "firstName": "Preston",
+      "lastName": "Walker"
+    },
+    "progress": {
+      "current": 97,
+      "total": 5367
+    },
+    "startedDate": "2018-11-20T14:50:44.000",
+    "completedDate": ""
+  }') ON CONFLICT DO NOTHING;
+
+
+
+
+
+
+
+
+
+
+
+
