0e5f9325b616c42744212a643cf9e0ac746ab9b4
Taras Spashchenko <Taras_Spashchenko@epam.com>
2018-12-16 22:32:31 +0200

    bugfix/MODSOURMAN-40 + Stub data for job executions

---
+++ src/main/java/org/folio/rest/impl/ModTenantAPI.java
@@ -0,0 +1,73 @@
+package org.folio.rest.impl;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.folio.rest.annotations.Validate;
+import org.folio.rest.jaxrs.model.TenantAttributes;
+import org.folio.rest.persist.PostgresClient;
+import org.folio.rest.tools.utils.TenantTool;
+
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+public class ModTenantAPI extends TenantAPI {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ModTenantAPI.class);
+
+  private static final String TEST_MODE = "test.mode";
+
+  private static final String SETUP_TEST_DATA_SQL = "templates/db_scripts/setup_test_data.sql";
+
+  private static final String TENANT_PLACEHOLDER = "${myuniversity}";
+
+  private static final String MODULE_PLACEHOLDER = "${mymodule}";
+
+  @Validate
+  @Override
+  public void postTenant(TenantAttributes entity, Map<String, String> headers, Handler<AsyncResult<Response>> handlers, Context context) {
+    super.postTenant(entity, headers, (ar) -> {
+      if (ar.failed()) {
+        handlers.handle(ar);
+      } else {
+        setupTestData(entity, headers, context).setHandler((event) -> {
+          handlers.handle(ar);
+        });
+      }
+    }, context);
+  }
+
+  private Future<List<String>> setupTestData(TenantAttributes entity, Map<String, String> headers, Context context) {
+    try {
+
+      if (!Boolean.TRUE.equals(Boolean.valueOf(System.getProperty(TEST_MODE, "false")))) {
+        return Future.succeededFuture();
+      }
+
+      String sqlScript = IOUtils.toString(this.getClass().getClassLoader().getResourceAsStream(SETUP_TEST_DATA_SQL), StandardCharsets.UTF_8.name());
+      if (StringUtils.isBlank(sqlScript)) {
+        return Future.succeededFuture();
+      }
+
+      String tenantId = TenantTool.calculateTenantId((String) headers.get("x-okapi-tenant"));
+      String moduleName = PostgresClient.getModuleName();
+
+      sqlScript = sqlScript.replace(TENANT_PLACEHOLDER, tenantId).replace(MODULE_PLACEHOLDER, moduleName);
+
+      Future<List<String>> future = Future.future();
+      PostgresClient.getInstance(context.owner()).runSQLFile(sqlScript, false, future::handle);
+
+      return future;
+    } catch (IOException e) {
+      return Future.failedFuture(e);
+    }
+  }
+
+}
