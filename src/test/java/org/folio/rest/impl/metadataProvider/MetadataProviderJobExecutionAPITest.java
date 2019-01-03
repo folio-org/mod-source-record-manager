6252423fa5e49083601ac4fea73615198319cd79
Kateryna Senchenko <kateryna_senchenko@epam.com>
2019-01-03 17:51:12 +0200

    JobExecutions filtered to exclude DISCARDED

--- src/test/java/org/folio/rest/impl/metadataProvider/MetadataProviderJobExecutionAPITest.java
+++ src/test/java/org/folio/rest/impl/metadataProvider/MetadataProviderJobExecutionAPITest.java
@@ -1,18 +1,23 @@
 package org.folio.rest.impl.metadataProvider;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.apache.http.HttpStatus;
 import org.folio.rest.impl.AbstractRestTest;
 import org.folio.rest.jaxrs.model.JobExecution;
+import org.folio.rest.jaxrs.model.StatusDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 /**
  * REST tests for MetadataProvider to manager JobExecution entities
@@ -64,6 +69,40 @@ public class MetadataProviderJobExecutionAPITest extends AbstractRestTest {
       .statusCode(HttpStatus.SC_OK)
       .body("jobExecutionDtos.size()", is(2))
       .body("totalRecords", is(expectedJobExecutionsNumber));
+  }
+
+  @Test
+  public void shouldNotReturnDiscardedInCollection() {
+    int numberOfFiles = 5;
+    int expectedNotDiscardedNumber = 2;
+    List<JobExecution> createdJobExecutions = constructAndPostInitJobExecutionRqDto(numberOfFiles).getJobExecutions();
+    List<JobExecution> children = createdJobExecutions.stream()
+      .filter(jobExec -> jobExec.getSubordinationType().equals(JobExecution.SubordinationType.CHILD)).collect(Collectors.toList());
+    StatusDto discardedStatus = new StatusDto().withStatus(StatusDto.Status.DISCARDED);
+
+    for (int i = 0; i < children.size() - expectedNotDiscardedNumber ; i++) {
+      updateJobExecutionStatus(children.get(i), discardedStatus)
+        .then()
+        .statusCode(HttpStatus.SC_OK);
+    }
+
+    RestAssured.given()
+      .spec(spec)
+      .when()
+      .get(GET_JOB_EXECUTIONS_PATH)
+      .then()
+      .statusCode(HttpStatus.SC_OK)
+      .body("jobExecutionDtos.size()", is(expectedNotDiscardedNumber))
+      .body("jobExecutionDtos*.status", not(StatusDto.Status.DISCARDED.name()))
+      .body("totalRecords", is(expectedNotDiscardedNumber));
+  }
+
+  private Response updateJobExecutionStatus(JobExecution jobExecution, StatusDto statusDto) {
+    return RestAssured.given()
+      .spec(spec)
+      .body(JsonObject.mapFrom(statusDto).toString())
+      .when()
+      .put(JOB_EXECUTION_PATH + jobExecution.getId() + "/status");
   }
 
 }
