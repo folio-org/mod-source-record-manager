9231a8a505e312450a8c9767d198e20cb81d0dad
oleksii_kuzminov <oleksii_kuzminov@epam.com>
2019-02-12 15:46:27 +0200

    merge conflicts

--- src/main/java/org/folio/services/FileExtensionServiceImpl.java
+++ src/main/java/org/folio/services/FileExtensionServiceImpl.java
@@ -148,4 +148,14 @@ public class FileExtensionServiceImpl implements FileExtensionService {
     return fileExtensionDao.getFileExtensions(query.toString(), 0, 1)
       .compose(collection -> Future.succeededFuture(collection.getTotalRecords() != 0));
   }
+
+  @Override
+  public Future<DataTypeCollection> getDataTypes() {
+    Future<DataTypeCollection> future = Future.future();
+    DataTypeCollection dataTypeCollection = new DataTypeCollection();
+    dataTypeCollection.setDataTypes(Arrays.asList(DataType.values()));
+    dataTypeCollection.setTotalRecords(DataType.values().length);
+    future.complete(dataTypeCollection);
+    return future;
+  }
 }
