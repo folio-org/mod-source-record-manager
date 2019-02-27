0cecc33c372a2be5e1b1eb14ba4dfdf7d17de65c
oleksii_kuzminov <oleksii_kuzminov@epam.com>
2019-02-27 16:54:15 +0200

    MODSOURMAN-70 Implement JobExecutionSourceChunk processing routine. Simplify logic in Change Engine Service

--- src/main/java/org/folio/services/ChangeEngineService.java
+++ src/main/java/org/folio/services/ChangeEngineService.java
@@ -3,15 +3,17 @@ package org.folio.services;
 import io.vertx.core.Future;
 import org.folio.dataimport.util.OkapiConnectionParams;
 import org.folio.rest.jaxrs.model.JobExecution;
+import org.folio.rest.jaxrs.model.RawRecordsDto;
 
 public interface ChangeEngineService {
 
   /**
-   * Load source records from mod-source-storage, parse them and save parsed result or errors to mod-source-storage
+   * Parse raw records and save parsed result or errors to mod-source-storage
    *
+   * @param chunk  - {@link RawRecordsDto} chunk with list of raw records for parse
    * @param job    - JobExecution which records should be parsed
    * @param params - OkapiConnectionParams to interact with external services
-   * @return - JobExecution with changed status and saved parsed records for it
+   * @return - {@link RawRecordsDto} that was parsed and saved into the source-record-storage
    */
-  Future<JobExecution> parseSourceRecordsForJobExecution(JobExecution job, OkapiConnectionParams params);
+  Future<RawRecordsDto> parseRawRecordsChunkForJobExecution(RawRecordsDto chunk, JobExecution job, OkapiConnectionParams params);
 }
