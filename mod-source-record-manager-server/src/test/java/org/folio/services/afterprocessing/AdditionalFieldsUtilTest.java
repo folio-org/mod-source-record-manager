f3de068877099e188f011d9f4f6d63057c1b1c17
Igor_Gorchakov <Igor_Gorchakov@epam.com>
2019-05-16 21:07:11 +0300

    Fixed tests

--- mod-source-record-manager-server/src/test/java/org/folio/services/afterprocessing/AdditionalFieldsUtilTest.java
+++ mod-source-record-manager-server/src/test/java/org/folio/services/afterprocessing/AdditionalFieldsUtilTest.java
@@ -1,58 +1,28 @@
 package org.folio.services.afterprocessing;
 
 
-import com.github.tomakehurst.wiremock.common.Slf4jNotifier;
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import io.vertx.core.Future;
-import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.unit.Async;
-import io.vertx.ext.unit.TestContext;
-import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.folio.rest.jaxrs.model.ParsedRecord;
 import org.folio.rest.jaxrs.model.Record;
-import org.junit.Before;
-import org.junit.Rule;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
-import static org.folio.dataimport.util.RestUtil.OKAPI_TENANT_HEADER;
-import static org.folio.dataimport.util.RestUtil.OKAPI_TOKEN_HEADER;
-import static org.folio.dataimport.util.RestUtil.OKAPI_URL_HEADER;
-
-@RunWith(VertxUnitRunner.class)
+@RunWith(BlockJUnit4ClassRunner.class)
 public class AdditionalFieldsUtilTest {
-
-  private static final String TENANT = "diku";
-  private static final String TOKEN = "token";
   private static final String PARSED_RECORD_PATH = "src/test/resources/org/folio/services/afterprocessing/parsedRecord.json";
-
-  @Rule
-  public WireMockRule mockServer =
-    new WireMockRule(WireMockConfiguration.wireMockConfig().dynamicPort().notifier(new Slf4jNotifier(true)));
   private AdditionalFieldsUtil additionalFieldsUtil = new AdditionalFieldsUtil();
-  private Vertx vertx = Vertx.vertx();
-  private Map<String, String> headers = new HashMap<>();
-
-  @Before
-  public void setUp() {
-    headers.put(OKAPI_URL_HEADER, "http://localhost:" + mockServer.port());
-    headers.put(OKAPI_TENANT_HEADER, TENANT);
-    headers.put(OKAPI_TOKEN_HEADER, TOKEN);
-  }
 
   @Test
-  public void shouldAddInstanceIdSubfield(TestContext testContext) throws IOException {
-    Async async = testContext.async();
+  public void shouldAddInstanceIdSubfield() throws IOException {
     // given
     String recordId = UUID.randomUUID().toString();
     String instanceId = UUID.randomUUID().toString();
@@ -64,28 +34,51 @@ public class AdditionalFieldsUtilTest {
     parsedRecord.setContent(parsedRecordContent);
     Record record = new Record().withId(recordId).withParsedRecord(parsedRecord);
     // when
-    Future<Record> future = additionalFieldsUtil.addInstanceIdToMarcRecord(record, instanceId);
+    boolean added = additionalFieldsUtil.addInstanceIdToMarcRecord(record, instanceId);
     // then
-    future.setHandler(ar -> {
-      testContext.assertTrue(ar.succeeded());
-      Record actualRecord = ar.result();
-      JsonObject content = new JsonObject((String) actualRecord.getParsedRecord().getContent());
-      JsonArray fields = content.getJsonArray("fields");
-      testContext.assertTrue(!fields.isEmpty());
-      for (int i = fields.size(); i-- > 0; ) {
-        JsonObject targetField = fields.getJsonObject(i);
-        if (targetField.containsKey(AdditionalFieldsConfig.TAG_999)) {
-          JsonArray subfields = targetField.getJsonObject(AdditionalFieldsConfig.TAG_999).getJsonArray("subfields");
-          for (int j = subfields.size(); j-- > 0; ) {
-            JsonObject targetSubfield = subfields.getJsonObject(j);
-            if (targetSubfield.containsKey("i")) {
-              String actualInstanceId = (String) targetSubfield.getValue("i");
-              testContext.assertEquals(instanceId, actualInstanceId);
-              async.complete();
-            }
+    Assert.assertTrue(added);
+    JsonObject content = new JsonObject((String) record.getParsedRecord().getContent());
+    JsonArray fields = content.getJsonArray("fields");
+    Assert.assertTrue(!fields.isEmpty());
+    for (int i = fields.size(); i-- > 0; ) {
+      JsonObject targetField = fields.getJsonObject(i);
+      if (targetField.containsKey(AdditionalFieldsConfig.TAG_999)) {
+        JsonArray subfields = targetField.getJsonObject(AdditionalFieldsConfig.TAG_999).getJsonArray("subfields");
+        for (int j = subfields.size(); j-- > 0; ) {
+          JsonObject targetSubfield = subfields.getJsonObject(j);
+          if (targetSubfield.containsKey("i")) {
+            String actualInstanceId = (String) targetSubfield.getValue("i");
+            Assert.assertEquals(instanceId, actualInstanceId);
           }
         }
       }
-    });
+    }
+  }
+
+  @Test
+  public void shouldNotAddInstanceIdSubfieldIfNoParsedRecordContent() {
+    // given
+    Record record = new Record();
+    String instanceId = UUID.randomUUID().toString();
+    // when
+    boolean added = additionalFieldsUtil.addInstanceIdToMarcRecord(record, instanceId);
+    // then
+    Assert.assertFalse(added);
+    Assert.assertNull(record.getParsedRecord());
+  }
+
+  @Test
+  public void shouldNotAddInstanceIdSubfieldIfNoFieldsInParsedRecordContent() {
+    // given
+    Record record = new Record();
+    record.setParsedRecord(new ParsedRecord().withContent(StringUtils.EMPTY));
+    String instanceId = UUID.randomUUID().toString();
+    // when
+    boolean added = additionalFieldsUtil.addInstanceIdToMarcRecord(record, instanceId);
+    // then
+    Assert.assertFalse(added);
+    Assert.assertNotNull(record.getParsedRecord());
+    Assert.assertNotNull(record.getParsedRecord().getContent());
+    Assert.assertEquals(StringUtils.EMPTY, record.getParsedRecord().getContent());
   }
 }
