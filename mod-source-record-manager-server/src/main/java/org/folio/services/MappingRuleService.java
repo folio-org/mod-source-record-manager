8896d581c88687e7724754fb3a9f59f74e02856b
Igor_Gorchakov <Igor_Gorchakov@epam.com>
2019-10-06 10:59:19 +0300

    Add javadoc

--- mod-source-record-manager-server/src/main/java/org/folio/services/MappingRuleService.java
+++ mod-source-record-manager-server/src/main/java/org/folio/services/MappingRuleService.java
@@ -5,13 +5,41 @@ import io.vertx.core.json.JsonObject;
 
 import java.util.Optional;
 
+/**
+ * Mapping rules service
+ */
 public interface MappingRuleService {
 
+  /**
+   * Returns rules in JsonObject
+   *
+   * @param tenantId tenant
+   * @return optional with rules in JsonObject
+   */
   Future<Optional<JsonObject>> get(String tenantId);
 
+  /**
+   * Saves default rules from resources classpath
+   *
+   * @param tenantId tenant
+   * @return future
+   */
   Future<Void> saveDefaultRules(String tenantId);
 
+  /**
+   * Updates default rules
+   *
+   * @param rules    rules
+   * @param tenantId tenant
+   * @return rules in JsonObject
+   */
   Future<JsonObject> update(String rules, String tenantId);
 
+  /**
+   * Updates existing rules with default rules
+   *
+   * @param tenantId tenant
+   * @return restored rules
+   */
   Future<JsonObject> restore(String tenantId);
 }
