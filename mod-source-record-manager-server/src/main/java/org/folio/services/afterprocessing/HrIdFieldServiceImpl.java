55f6774e7f5720303ca3506fff7b9035b28ed107
Oleksii Kuzminov <40833111+oleksiikuzminov@users.noreply.github.com>
2020-04-16 23:34:10 +0300

    Moddicore 43: Fix formatting of 035 field constructed from incoming 001 (#268)
    * MODDICORE-43
    SRS MARC Bib: Fix formatting of 035 field constructed from incoming 001
    
    (cherry picked from commit 498361eb0b31a6cff8cd5a785a1052beba28d85a)

--- mod-source-record-manager-server/src/main/java/org/folio/services/afterprocessing/HrIdFieldServiceImpl.java
+++ mod-source-record-manager-server/src/main/java/org/folio/services/afterprocessing/HrIdFieldServiceImpl.java
@@ -10,6 +10,8 @@ import java.util.List;
 import java.util.Map;
 
 import static org.folio.services.afterprocessing.AdditionalFieldsUtil.addControlledFieldToMarcRecord;
+import static org.folio.services.afterprocessing.AdditionalFieldsUtil.addDataFieldToMarcRecord;
+import static org.folio.services.afterprocessing.AdditionalFieldsUtil.isFieldExist;
 import static org.folio.services.afterprocessing.AdditionalFieldsUtil.removeField;
 
 @Service
@@ -17,6 +19,8 @@ public class HrIdFieldServiceImpl implements HrIdFieldService {
 
   private static final String HR_ID_FROM_FIELD = "001";
   private static final String HR_ID_TO_FIELD = "035";
+  private static final char HR_ID_FIELD_SUB = 'a';
+  private static final char HR_ID_FIELD_IND = ' ';
 
   public void moveHrIdFieldsAfterMapping(Map<Instance, Record> map) {
     map.entrySet().stream().parallel().forEach(entry -> {
@@ -36,8 +40,8 @@ public class HrIdFieldServiceImpl implements HrIdFieldService {
   public void fillHrIdFieldInMarcRecord(List<Pair<Record, Instance>> list) {
     list.stream().parallel().forEach(recordInstancePair -> {
       String hrId = recordInstancePair.getValue().getHrid();
-      if (StringUtils.isNotEmpty(hrId)) {
-        addControlledFieldToMarcRecord(recordInstancePair.getKey(), HR_ID_FROM_FIELD, hrId);
+      if (StringUtils.isNotEmpty(hrId) && !isFieldExist(recordInstancePair.getKey(), HR_ID_FROM_FIELD, HR_ID_FIELD_SUB, hrId)) {
+        addDataFieldToMarcRecord(recordInstancePair.getKey(), HR_ID_FROM_FIELD, HR_ID_FIELD_IND, HR_ID_FIELD_IND, HR_ID_FIELD_SUB, hrId);
       }
     });
   }
