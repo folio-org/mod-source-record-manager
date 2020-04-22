090d4ea561259739c4f940d37fb7ac9375cdcd94
Kateryna Senchenko <Kateryna_Senchenko@epam.com>
2020-04-22 21:51:17 +0300

    Updated pubsub client and data-import-processing-core dependencies

--- mod-source-record-manager-server/src/main/java/org/folio/services/journal/JournalRecordMapperException.java
+++ mod-source-record-manager-server/src/main/java/org/folio/services/journal/JournalRecordMapperException.java
@@ -8,4 +8,9 @@ public class JournalRecordMapperException extends Exception {
   public JournalRecordMapperException(String message) {
     super(message);
   }
+
+  public JournalRecordMapperException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
 }
