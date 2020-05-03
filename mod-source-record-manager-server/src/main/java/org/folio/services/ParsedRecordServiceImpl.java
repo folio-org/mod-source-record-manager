311cec2adecc1b16ad4c2c1b6849500ef65aa971
Kateryna Senchenko <kateryna_senchenko@epam.com>
2020-05-03 14:23:01 +0300

    Added event sending

--- mod-source-record-manager-server/src/main/java/org/folio/services/ParsedRecordServiceImpl.java
+++ mod-source-record-manager-server/src/main/java/org/folio/services/ParsedRecordServiceImpl.java
@@ -3,29 +3,55 @@ package org.folio.services;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.UUID;
 import javax.ws.rs.NotFoundException;
 import org.folio.dataimport.util.OkapiConnectionParams;
 import org.folio.dataimport.util.Try;
+import org.folio.processing.mapping.defaultmapper.processor.parameters.MappingParameters;
+import org.folio.rest.client.DataImportProfilesClient;
 import org.folio.rest.client.SourceStorageClient;
+import org.folio.rest.jaxrs.model.DataImportEventPayload;
+import org.folio.rest.jaxrs.model.Event;
+import org.folio.rest.jaxrs.model.EventMetadata;
 import org.folio.rest.jaxrs.model.ParsedRecordDto;
+import org.folio.rest.jaxrs.model.ProfileSnapshotWrapper;
 import org.folio.rest.jaxrs.model.Record;
 import org.folio.rest.jaxrs.model.Snapshot;
 import org.folio.rest.jaxrs.model.SourceRecord;
+import org.folio.services.mappers.processor.MappingParametersProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import static java.lang.String.format;
 import static org.folio.HttpStatus.HTTP_CREATED;
 import static org.folio.HttpStatus.HTTP_NOT_FOUND;
 import static org.folio.HttpStatus.HTTP_OK;
+import static org.folio.rest.jaxrs.model.EntityType.MARC_BIBLIOGRAPHIC;
+import static org.folio.util.pubsub.PubSubClientUtils.constructModuleName;
+import static org.folio.util.pubsub.PubSubClientUtils.sendEventMessage;
 
 @Service
 public class ParsedRecordServiceImpl implements ParsedRecordService {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(ParsedRecordServiceImpl.class);
+
+  private static final String DEFAULT_JOB_PROFILE_ID = "22fafcc3-f582-493d-88b0-3c538480cd83";
+  private static final String QM_RECORD_UPDATED_EVENT_TYPE = "QM_SRS_MARC_BIB_RECORD_UPDATED";
+
+  private MappingParametersProvider mappingParametersProvider;
+  private MappingRuleService mappingRuleService;
+
+  public ParsedRecordServiceImpl(@Autowired MappingParametersProvider mappingParametersProvider,
+                                 @Autowired MappingRuleService mappingRuleService) {
+    this.mappingParametersProvider = mappingParametersProvider;
+    this.mappingRuleService = mappingRuleService;
+  }
 
   @Override
   public Future<ParsedRecordDto> getRecordByInstanceId(String instanceId, OkapiConnectionParams params) {
@@ -57,8 +83,11 @@ public class ParsedRecordServiceImpl implements ParsedRecordService {
     return getRecordById(parsedRecordDto.getId(), params)
       .compose(oldRecord -> createSnapshot(params)
         .compose(snapshot -> createNewRecord(snapshot, oldRecord, parsedRecordDto, params))
-        .compose(ar -> updateRecord(oldRecord.withState(Record.State.OLD), params))
-        .map(ar -> true));
+        .compose(newRecord -> {
+          sendEventWithUpdatedRecord(newRecord, params);
+          return updateRecord(oldRecord.withState(Record.State.OLD), params)
+            .map(ar -> true);
+        }));
   }
 
   private ParsedRecordDto mapSourceRecordToParsedRecordDto(Buffer body) {
@@ -177,6 +206,82 @@ public class ParsedRecordServiceImpl implements ParsedRecordService {
       LOGGER.error("Failed to update Record in SRS", e);
       promise.fail(e);
     }
+    return promise.future();
+  }
+
+  private Future<Void> sendEventWithUpdatedRecord(Record record, OkapiConnectionParams params) {
+    return mappingParametersProvider.get(record.getSnapshotId(), params)
+      .compose(mappingParameters -> mappingRuleService.get(params.getTenantId())
+        .compose(rulesOptional -> {
+          if (rulesOptional.isPresent()) {
+            return createJobProfileSnapshotWrapper(DEFAULT_JOB_PROFILE_ID, params)
+              .compose(profileSnapshotWrapper -> sendEvent(record, profileSnapshotWrapper, rulesOptional.get(), mappingParameters, params));
+          } else {
+            return Future.failedFuture(format("Can not send %s event, no mapping rules found for tenant %s", QM_RECORD_UPDATED_EVENT_TYPE, params.getTenantId()));
+          }
+        }));
+  }
+
+  private Future<ProfileSnapshotWrapper> createJobProfileSnapshotWrapper(String jobProfileId, OkapiConnectionParams params) {
+    Promise<ProfileSnapshotWrapper> promise = Promise.promise();
+    DataImportProfilesClient client = new DataImportProfilesClient(params.getOkapiUrl(), params.getTenantId(), params.getToken());
+
+    client.postDataImportProfilesJobProfileSnapshotsById(jobProfileId, response -> {
+      if (response.statusCode() == HTTP_CREATED.toInt()) {
+        response.bodyHandler(body ->
+          promise.handle(Try.itGet(() -> body.toJsonObject().mapTo(ProfileSnapshotWrapper.class))));
+      } else {
+        String message = format("Failed to create ProfileSnapshotWrapper by JobProfile id %s, response code %s, %s",
+          jobProfileId, response.statusCode(), response.statusMessage());
+        LOGGER.error(message);
+        promise.fail(message);
+      }
+    });
+    return promise.future();
+  }
+
+  private Future<Void> sendEvent(Record updatedRecord, ProfileSnapshotWrapper profileSnapshotWrapper,
+                                 JsonObject mappingRules, MappingParameters mappingParameters, OkapiConnectionParams params) {
+    Promise<Void> promise = Promise.promise();
+    HashMap<String, String> dataImportEventPayloadContext = new HashMap<>();
+    dataImportEventPayloadContext.put(MARC_BIBLIOGRAPHIC.value(), Json.encode(updatedRecord));
+    dataImportEventPayloadContext.put("MAPPING_RULES", mappingRules.encode());
+    dataImportEventPayloadContext.put("MAPPING_PARAMS", Json.encode(mappingParameters));
+
+    DataImportEventPayload dataImportEventPayload = new DataImportEventPayload()
+      .withEventType(QM_RECORD_UPDATED_EVENT_TYPE)
+      .withProfileSnapshot(profileSnapshotWrapper)
+      .withCurrentNode(profileSnapshotWrapper.getChildSnapshotWrappers().get(0))
+      .withJobExecutionId(updatedRecord.getSnapshotId())
+      .withContext(dataImportEventPayloadContext)
+      .withOkapiUrl(params.getOkapiUrl())
+      .withTenant(params.getTenantId())
+      .withToken(params.getToken());
+
+    Event event = new Event()
+      .withId(UUID.randomUUID().toString())
+      .withEventType(QM_RECORD_UPDATED_EVENT_TYPE)
+      .withEventPayload(Json.encode(dataImportEventPayload))
+      .withEventMetadata(new EventMetadata()
+        .withTenantId(params.getTenantId())
+        .withEventTTL(1)
+        .withPublishedBy(constructModuleName()));
+
+    org.folio.rest.util.OkapiConnectionParams connectionParams = new org.folio.rest.util.OkapiConnectionParams();
+    connectionParams.setOkapiUrl(params.getOkapiUrl());
+    connectionParams.setToken(params.getToken());
+    connectionParams.setTenantId(params.getTenantId());
+    connectionParams.setVertx(params.getVertx());
+
+    sendEventMessage(event, connectionParams)
+      .whenComplete((ar, throwable) -> {
+        if (throwable == null) {
+          promise.complete();
+        } else {
+          LOGGER.error("Error during event sending: {}", throwable, event);
+          promise.fail(throwable);
+        }
+      });
     return promise.future();
   }
 
