91501826bfee795f02edad9d59e32b0279c6e7bd
William Welling <wwelling@library.tamu.edu>
2021-02-22 09:53:44 -0600

    use static map from record type to created event type

--- mod-source-record-manager-server/src/main/java/org/folio/verticle/consumers/StoredMarcChunksKafkaHandler.java
+++ mod-source-record-manager-server/src/main/java/org/folio/verticle/consumers/StoredMarcChunksKafkaHandler.java
@@ -2,6 +2,7 @@ package org.folio.verticle.consumers;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -27,11 +28,18 @@ import io.vertx.kafka.client.producer.KafkaHeader;
 
 import static org.folio.rest.jaxrs.model.DataImportEventTypes.DI_EDIFACT_RECORD_CREATED;
 import static org.folio.rest.jaxrs.model.DataImportEventTypes.DI_SRS_MARC_BIB_RECORD_CREATED;
+import static org.folio.rest.jaxrs.model.Record.RecordType.EDIFACT;
+import static org.folio.rest.jaxrs.model.Record.RecordType.MARC;
 
 @Component
 @Qualifier("StoredMarcChunksKafkaHandler")
 public class StoredMarcChunksKafkaHandler implements AsyncRecordHandler<String, String> {
   private static final Logger LOGGER = LogManager.getLogger();
+
+  private static final Map<RecordType, DataImportEventTypes> RECORD_TYPE_TO_EVENT_TYPE = Map.of(
+    MARC, DI_SRS_MARC_BIB_RECORD_CREATED,
+    EDIFACT, DI_EDIFACT_RECORD_CREATED
+  );
 
   private RecordsPublishingService recordsPublishingService;
   private Vertx vertx;
@@ -55,17 +63,15 @@ public class StoredMarcChunksKafkaHandler implements AsyncRecordHandler<String, 
       String unzipped = ZIPArchiver.unzip(event.getEventPayload());
       RecordsBatchResponse recordsBatchResponse = new JsonObject(unzipped).mapTo(RecordsBatchResponse.class);
       List<Record> storedRecords = recordsBatchResponse.getRecords();
-      // default to previous static value
-      String eventType = DI_SRS_MARC_BIB_RECORD_CREATED.value();
-      // we only know record type by inspecting the records, assuming records are homogeneous type
-      if (!storedRecords.isEmpty()) {
-        RecordType recordType = storedRecords.get(0).getRecordType();
-        eventType = RecordTypeToCreatedEventType.valueOf(recordType.value()).getCreatedEventType();
-      }
+
+      // we only know record type by inspecting the records, assuming records are homogeneous type and defaulting to previous static value
+      DataImportEventTypes eventType = !storedRecords.isEmpty() && RECORD_TYPE_TO_EVENT_TYPE.containsKey(storedRecords.get(0).getRecordType())
+        ? RECORD_TYPE_TO_EVENT_TYPE.get(storedRecords.get(0).getRecordType())
+        : DI_SRS_MARC_BIB_RECORD_CREATED;
 
       LOGGER.debug("RecordsBatchResponse has been received, starting processing correlationId: {} chunkNumber: {}", correlationId, chunkNumber);
       return recordsPublishingService.sendEventsWithRecords(storedRecords, okapiConnectionParams.getHeaders().get("jobExecutionId"),
-        okapiConnectionParams, eventType)
+        okapiConnectionParams, eventType.value())
         .compose(b -> {
           LOGGER.debug("RecordsBatchResponse processing has been completed correlationId: {} chunkNumber: {}",correlationId, chunkNumber);
           return Future.succeededFuture(correlationId);
@@ -77,25 +83,6 @@ public class StoredMarcChunksKafkaHandler implements AsyncRecordHandler<String, 
       LOGGER.error("Can't process kafka record: ", e);
       return Future.failedFuture(e);
     }
-  }
-
-  /**
-   * Enum mapping from {@link RecordType} to {@link DataImportEventTypes} for created events.
-   */
-  private enum RecordTypeToCreatedEventType {
-    MARC(DI_SRS_MARC_BIB_RECORD_CREATED),
-    EDIFACT(DI_EDIFACT_RECORD_CREATED);
-
-    private String createdEventType;
-
-    private RecordTypeToCreatedEventType(DataImportEventTypes createdEventType) {
-      this.createdEventType = createdEventType.value();
-    }
-
-    public String getCreatedEventType() {
-      return this.createdEventType;
-    }
-
   }
 
 }
