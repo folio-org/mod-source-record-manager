b8733fcd78cb6358ad0bdf4a635c2b861a1abed3
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2021-03-26 15:56:37 +0200

    MODSOURMAN-411 Dynamicaly define the payload of Data Import events depending on MARC Record type (Bib, Authority, Holding) (#397)

---
+++ mod-source-record-manager-server/src/test/java/org/folio/services/DataImportPayloadContextBuilderImplTest.java
@@ -0,0 +1,145 @@
+package org.folio.services;
+
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import org.folio.dataimport.util.marc.MarcRecordAnalyzer;
+import org.folio.dataimport.util.marc.MarcRecordType;
+import org.folio.processing.mapping.defaultmapper.processor.parameters.MappingParameters;
+import org.folio.rest.jaxrs.model.ParsedRecord;
+import org.folio.rest.jaxrs.model.Record;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.folio.rest.jaxrs.model.EntityType.EDIFACT_INVOICE;
+import static org.folio.rest.jaxrs.model.EntityType.MARC_AUTHORITY;
+import static org.folio.rest.jaxrs.model.EntityType.MARC_BIBLIOGRAPHIC;
+import static org.folio.rest.jaxrs.model.EntityType.MARC_HOLDINGS;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DataImportPayloadContextBuilderImplTest {
+
+  @Mock
+  private MarcRecordAnalyzer marcRecordAnalyzer;
+  @InjectMocks
+  private DataImportPayloadContextBuilderImpl builder;
+
+  private Record record;
+  private JsonObject rules;
+  private MappingParameters params;
+
+
+  @Before
+  public void setUp() throws Exception {
+    record = new Record();
+    rules = new JsonObject();
+    params = new MappingParameters();
+  }
+
+  @Test
+  public void shouldBuildContextForMarcAuthorityRecord() {
+    ParsedRecord parsedRecord = parsedRecord("{\"leader\":\"authority\"}");
+    record.setRecordType(Record.RecordType.MARC);
+    record.setParsedRecord(parsedRecord);
+
+    when(marcRecordAnalyzer.process(toJson(parsedRecord))).thenReturn(MarcRecordType.AUTHORITY);
+
+    HashMap<String, String> context = builder.buildFrom(record, rules, params);
+
+    assertEquals(Map.of(MARC_AUTHORITY.value(), Json.encode(record)), context);
+  }
+
+  @Test
+  public void shouldBuildContextForMarcBibRecord() {
+    ParsedRecord parsedRecord = parsedRecord("{\"leader\":\"bibliographic\"}");
+    record.setRecordType(Record.RecordType.MARC);
+    record.setParsedRecord(parsedRecord);
+
+    when(marcRecordAnalyzer.process(toJson(parsedRecord))).thenReturn(MarcRecordType.BIB);
+
+    HashMap<String, String> context = builder.buildFrom(record, rules, params);
+
+    assertEquals(Map.of(
+        MARC_BIBLIOGRAPHIC.value(), Json.encode(record),
+        "MAPPING_RULES", rules.encode(),
+        "MAPPING_PARAMS", Json.encode(params)),
+        context);
+  }
+
+  @Test
+  public void shouldBuildContextForMarcHoldingRecord() {
+    ParsedRecord parsedRecord = parsedRecord("{\"leader\":\"holding\"}");
+    record.setRecordType(Record.RecordType.MARC);
+    record.setParsedRecord(parsedRecord);
+
+    when(marcRecordAnalyzer.process(toJson(parsedRecord))).thenReturn(MarcRecordType.HOLDING);
+
+    HashMap<String, String> context = builder.buildFrom(record, rules, params);
+
+    assertEquals(Map.of(
+        MARC_HOLDINGS.value(), Json.encode(record),
+        "MAPPING_RULES", rules.encode(),
+        "MAPPING_PARAMS", Json.encode(params)),
+        context);
+  }
+
+  @Test
+  public void shouldBuildContextForEdifactInvoice() {
+    record.setRecordType(Record.RecordType.EDIFACT);
+
+    HashMap<String, String> context = builder.buildFrom(record, rules, params);
+
+    assertEquals(Map.of(
+        EDIFACT_INVOICE.value(), Json.encode(record),
+        "MAPPING_RULES", rules.encode(),
+        "MAPPING_PARAMS", Json.encode(params)),
+        context);
+  }
+
+  @Test
+  public void shouldThrowNPEIfParsedRecordIsNullAndRecordIsMarc() {
+    record.setRecordType(Record.RecordType.MARC);
+
+    assertThrows("Parsed record is null", NullPointerException.class,
+        () -> builder.buildFrom(record, rules, params));
+  }
+
+  @Test
+  public void shouldThrowNPEIfParsedRecordContentIsNullAndRecordIsMarc() {
+    record.setRecordType(Record.RecordType.MARC);
+    record.setParsedRecord(new ParsedRecord());
+
+    assertThrows("Parsed record content is null", NullPointerException.class,
+        () -> builder.buildFrom(record, rules, params));
+  }
+
+  @Test
+  public void shouldThrowExceptionIfMarcTypeUnsupported() {
+    ParsedRecord parsedRecord = parsedRecord("{\"leader\":\"NA\"}");
+    record.setRecordType(Record.RecordType.MARC);
+    record.setParsedRecord(parsedRecord);
+
+    when(marcRecordAnalyzer.process(toJson(parsedRecord))).thenReturn(MarcRecordType.NA);
+
+    assertThrows("Unsupported Marc record type", IllegalStateException.class,
+        () -> builder.buildFrom(record, rules, params));
+  }
+
+  private static ParsedRecord parsedRecord(String content) {
+    return new ParsedRecord().withContent(content);
+  }
+  
+  private static JsonObject toJson(ParsedRecord parsedRecord) {
+    return new JsonObject(parsedRecord.getContent().toString());
+  }
+
+}
