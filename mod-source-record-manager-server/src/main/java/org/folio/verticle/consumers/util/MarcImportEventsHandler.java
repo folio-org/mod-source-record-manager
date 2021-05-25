2f4fd808095c6ed9410639475a753ab239944539
Natalia_Zaitseva <Natalia_Zaitseva1@epam.com>
2021-05-25 13:20:28 +0300

    updated JournalParams with DI_SRS_MARC_AUTHORITY_RECORD_CREATED event

--- mod-source-record-manager-server/src/main/java/org/folio/verticle/consumers/util/MarcImportEventsHandler.java
+++ mod-source-record-manager-server/src/main/java/org/folio/verticle/consumers/util/MarcImportEventsHandler.java
@@ -1,6 +1,8 @@
 package org.folio.verticle.consumers.util;
 
 import io.vertx.core.json.JsonObject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.DataImportEventPayload;
 import org.folio.rest.jaxrs.model.JournalRecord;
 import org.folio.services.journal.JournalRecordMapperException;
@@ -8,11 +10,12 @@ import org.folio.services.journal.JournalService;
 import org.folio.services.journal.JournalUtil;
 
 public class MarcImportEventsHandler implements SpecificEventHandler {
+  private static final Logger LOGGER = LogManager.getLogger();
 
   @Override
   public void handle(JournalService journalService, DataImportEventPayload eventPayload, String tenantId)
     throws JournalRecordMapperException {
-
+    LOGGER.info("LOGGING EVENT TYPE {}", eventPayload.getEventType());
     JournalParams journalParams =
       JournalParams.JournalParamsEnum.getValue(eventPayload.getEventType()).getJournalParams(eventPayload);
 
