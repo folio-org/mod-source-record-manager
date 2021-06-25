cebc959aec8bd65ade98d9b9f7283d93e0231ba6
natalia_zaitseva <natalia_zaitseva1@epam.com>
2021-06-25 13:23:15 +0300

    migration script for marc

--- mod-source-record-manager-server/src/main/resources/templates/db_scripts/migrate_data_type_to_marc.sql
+++ mod-source-record-manager-server/src/main/resources/templates/db_scripts/migrate_data_type_to_marc.sql
@@ -2,3 +2,9 @@
 UPDATE ${myuniversity}_${mymodule}.job_executions
 SET jsonb = jsonb_set(jsonb, '{jobProfileInfo, dataType}', '"MARC"')
 WHERE jsonb -> 'jobProfileInfo' ->>'dataType' IN ('MARC_BIB', 'MARC_AUTHORITY', 'MARC_HOLDINGS');
+
+
+UPDATE ${myuniversity}_${mymodule}.job_executions
+SET jsonb = jsonb_set(jsonb, '{jobProfileSnapshotWrapper, content, dataType}', '"MARC"')
+WHERE jsonb -> 'jobProfileSnapshotWrapper' -> 'content' ->>'dataType' IN ('MARC_BIB', 'MARC_AUTHORITY', 'MARC_HOLDINGS');
+
