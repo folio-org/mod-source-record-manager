a5451eca2b6469bbe79f9f8cbd4c6bf897807372
Pavlo Smahin <Pavlo_Smahin@epam.com>
2021-07-21 13:04:52 +0300

    MODSOURMAN-516 Add tests for QuickMarcUpdateKafkaHandler

--- mod-source-record-manager-server/src/main/java/org/folio/services/QuickMarcEventProducerServiceImpl.java
+++ mod-source-record-manager-server/src/main/java/org/folio/services/QuickMarcEventProducerServiceImpl.java
@@ -4,6 +4,7 @@ import static org.folio.services.util.EventHandlingUtil.createEvent;
 import static org.folio.services.util.EventHandlingUtil.createProducer;
 import static org.folio.services.util.EventHandlingUtil.createProducerRecord;
 import static org.folio.services.util.EventHandlingUtil.createTopicName;
+import static org.folio.verticle.consumers.util.QMEventTypes.QM_COMPLETED;
 import static org.folio.verticle.consumers.util.QMEventTypes.QM_RECORD_UPDATED;
 
 import java.util.HashMap;
@@ -30,6 +31,7 @@ public class QuickMarcEventProducerServiceImpl implements QuickMarcEventProducer
   public QuickMarcEventProducerServiceImpl(KafkaConfig kafkaConfig) {
     this.kafkaConfig = kafkaConfig;
     kafkaProducers.put(QM_RECORD_UPDATED.name(), createProducer(QM_RECORD_UPDATED.name(), kafkaConfig));
+    kafkaProducers.put(QM_COMPLETED.name(), createProducer(QM_COMPLETED.name(), kafkaConfig));
   }
 
   @Override
