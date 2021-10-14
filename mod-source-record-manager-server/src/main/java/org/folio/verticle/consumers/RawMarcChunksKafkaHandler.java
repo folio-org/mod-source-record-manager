6e0100a88746773f031a31c723d110a9ceb2e2dc
Khamidulla Abdulkhakimov <Khamidulla_Abdulkhakimov@epam.com>
2021-10-14 17:18:57 +0500

    MODSOURMAN-582 Handlers are fully covered with tests

--- mod-source-record-manager-server/src/main/java/org/folio/verticle/consumers/RawMarcChunksKafkaHandler.java
+++ mod-source-record-manager-server/src/main/java/org/folio/verticle/consumers/RawMarcChunksKafkaHandler.java
@@ -18,7 +18,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.List;
 
 @Component
