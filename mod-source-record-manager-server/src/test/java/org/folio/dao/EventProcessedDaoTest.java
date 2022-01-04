5df7e6d21fb156d735079ada41bc196b42fc5dc0
Serhii Nosko <Serhii_Nosko@epam.com>
2022-01-04 16:53:26 +0200

    Add dao test

---
+++ mod-source-record-manager-server/src/test/java/org/folio/dao/EventProcessedDaoTest.java
@@ -0,0 +1,66 @@
+package org.folio.dao;
+
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.pgclient.PgException;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+import org.folio.dao.util.PostgresClientFactory;
+import org.folio.rest.impl.AbstractRestTest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RunWith(VertxUnitRunner.class)
+public class EventProcessedDaoTest extends AbstractRestTest {
+
+  @Spy
+  private PostgresClientFactory postgresClientFactory = new PostgresClientFactory(Vertx.vertx());
+  @InjectMocks
+  private EventProcessedDaoImpl eventProcessedDao = new EventProcessedDaoImpl(postgresClientFactory);
+
+  @Before
+  public void setUp(TestContext context) throws IOException {
+    super.setUp(context);
+  }
+
+  @Test
+  public void shouldSaveEventProcessed(TestContext context) {
+    String eventId = UUID.randomUUID().toString();
+    String handlerId = UUID.randomUUID().toString();
+    Async async = context.async();
+    Future<RowSet<Row>> saveFuture = eventProcessedDao.save(eventId, handlerId, TENANT_ID);
+
+    saveFuture.onComplete(ar -> {
+      context.assertTrue(ar.succeeded());
+      context.assertEquals(1, ar.result().rowCount());
+      async.complete();
+    });
+  }
+
+  @Test
+  public void shouldThrowConstraintViolation(TestContext context) {
+    String eventId = UUID.randomUUID().toString();
+    String handlerId = UUID.randomUUID().toString();
+    Async async = context.async();
+
+    Future<RowSet<Row>> saveFuture = eventProcessedDao.save(eventId, handlerId, TENANT_ID);
+    saveFuture.onComplete(ar -> {
+      Future<RowSet<Row>> reSaveFuture = eventProcessedDao.save(eventId, handlerId, TENANT_ID);
+      reSaveFuture.onComplete(re -> {
+        context.assertTrue(re.failed());
+        context.assertTrue(re.cause() instanceof  PgException);
+        context.assertEquals("ERROR: duplicate key value violates unique constraint \"events_processed_pkey\" (23505)", re.cause().getMessage());
+        async.complete();
+      });
+    });
+  }
+}
