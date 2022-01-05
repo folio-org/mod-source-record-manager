d7447010aeb2015b9e4b0a0aa43c5607bb4062e5
Pavlo Smahin <Pavlo_Smahin@epam.com>
2022-01-05 15:47:17 +0200

    MODSOURMAN-656 Update JournalParams

--- mod-source-record-manager-server/src/main/java/org/folio/verticle/consumers/util/JournalParams.java
+++ mod-source-record-manager-server/src/main/java/org/folio/verticle/consumers/util/JournalParams.java
@@ -44,6 +44,14 @@ public class JournalParams {
           JournalRecord.ActionStatus.COMPLETED));
       }
     },
+    DI_SRS_MARC_AUTHORITY_RECORD_UPDATED {
+      @Override
+      public Optional<JournalParams> getJournalParams(DataImportEventPayload eventPayload) {
+        return Optional.of(new JournalParams(JournalRecord.ActionType.UPDATE,
+          JournalRecord.EntityType.MARC_AUTHORITY,
+          JournalRecord.ActionStatus.COMPLETED));
+      }
+    },
     DI_SRS_MARC_BIB_RECORD_MODIFIED {
       @Override
       public Optional<JournalParams> getJournalParams(DataImportEventPayload eventPayload) {
