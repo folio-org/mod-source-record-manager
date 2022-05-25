d75c05a305d72be403f052624118d0c8774ccd68
Shans Kaluhin <88006020+Shans-Kaluhin@users.noreply.github.com>
2022-05-25 16:37:05 +0300

    MODSOURMAN-797 Add userId to kafkaHeaders (#642)
    * Add userId to kafkaHeader

--- mod-source-record-manager-server/src/main/java/org/folio/services/ChangeEngineServiceImpl.java
+++ mod-source-record-manager-server/src/main/java/org/folio/services/ChangeEngineServiceImpl.java
@@ -13,7 +13,6 @@ import org.folio.rest.jaxrs.model.ExternalIdsHolder;
 import org.folio.rest.jaxrs.model.InitialRecord;
 import org.folio.rest.jaxrs.model.JobExecution;
 import org.folio.rest.jaxrs.model.JobExecutionSourceChunk;
-import org.folio.rest.jaxrs.model.Metadata;
 import org.folio.rest.jaxrs.model.ParsedRecord;
 import org.folio.rest.jaxrs.model.ProfileSnapshotWrapper;
 import org.folio.rest.jaxrs.model.RawRecord;
@@ -190,7 +189,6 @@ public class ChangeEngineServiceImpl implements ChangeEngineService {
 
   private Future<Boolean> updateRecords(List<Record> records, JobExecution jobExecution, OkapiConnectionParams params) {
     LOGGER.info("Records have not been saved in record-storage, because job contains action for Marc update");
-    records.forEach(parsedRecord -> parsedRecord.setMetadata(new Metadata().withUpdatedByUserId(jobExecution.getUserId())));
     return recordsPublishingService
       .sendEventsWithRecords(records, jobExecution.getId(), params, DI_MARC_FOR_UPDATE_RECEIVED.value());
   }
@@ -553,7 +551,6 @@ public class ChangeEngineServiceImpl implements ChangeEngineService {
     if (CollectionUtils.isEmpty(parsedRecords)) {
       return Future.succeededFuture();
     }
-    parsedRecords.forEach(parsedRecord -> parsedRecord.setMetadata(new Metadata().withCreatedByUserId(jobExecution.getUserId())));
     RecordCollection recordCollection = new RecordCollection()
       .withRecords(parsedRecords)
       .withTotalRecords(parsedRecords.size());
