9d4ba7330469cfb6a09ecec746e85087b8bbc602
Roman Chernetskyi <Roman_Chernetskyi@epam.com>
2022-10-05 16:48:11 +0300

    [MODSOURMAN-883] Upgrade to RMB v35.0.0

--- mod-source-record-manager-server/src/main/java/org/folio/dao/MappingRulesSnapshotDaoImpl.java
+++ mod-source-record-manager-server/src/main/java/org/folio/dao/MappingRulesSnapshotDaoImpl.java
@@ -38,7 +38,7 @@ public class MappingRulesSnapshotDaoImpl implements MappingRulesSnapshotDao {
     Promise<RowSet<Row>> promise = Promise.promise();
     String query = format(SELECT_QUERY, convertToPsqlStandard(tenantId), TABLE_NAME);
     Tuple queryParams = Tuple.of(UUID.fromString(jobExecutionId));
-    pgClientFactory.createInstance(tenantId).select(query, queryParams, promise);
+    pgClientFactory.createInstance(tenantId).selectRead(query, queryParams, promise);
     return promise.future().map(resultSet -> {
       if (resultSet.rowCount() == 0 || resultSet.iterator().next().getValue(RULES_FIELD) == null) {
         return Optional.empty();
