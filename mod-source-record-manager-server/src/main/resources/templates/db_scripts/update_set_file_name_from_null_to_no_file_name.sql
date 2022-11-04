8925f5978b620f926d483adb53cabacb7d970923
Kyrylo_Shulzhenko <Kyrylo_Shulzhenko@epam.com>
2022-11-04 16:41:24 +0200

    [MODSOURMAN-894]Logs for "No file name" sort at the end of the file name alpha list instead of in the N's

--- mod-source-record-manager-server/src/main/resources/templates/db_scripts/update_set_file_name_from_null_to_no_file_name.sql
+++ mod-source-record-manager-server/src/main/resources/templates/db_scripts/update_set_file_name_from_null_to_no_file_name.sql
@@ -1,6 +1,5 @@
-update diku_mod_source_record_manager.job_execution SET file_name = 'No file name' WHERE file_name IS NULL;
-ALTER TABLE diku_mod_source_record_manager.job_execution ALTER COLUMN file_name SET NOT NULL;
-update diku_mod_source_record_manager.job_execution SET source_path = 'No file name' WHERE source_path IS NULL;
-ALTER TABLE diku_mod_source_record_manager.job_execution ALTER COLUMN source_path SET NOT NULL;
-
+update ${myuniversity}_${mymodule}.job_execution SET file_name = 'No file name' WHERE file_name IS NULL;
+ALTER TABLE ${myuniversity}_${mymodule}.job_execution ALTER COLUMN file_name SET NOT NULL;
+update ${myuniversity}_${mymodule}.job_execution SET source_path = 'No file name' WHERE source_path IS NULL;
+ALTER TABLE ${myuniversity}_${mymodule}.job_execution ALTER COLUMN source_path SET NOT NULL;
 
