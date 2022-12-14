368e04b76c56c41ac90e48903ed0ecc574d61e54
Shans Kaluhin <88006020+Shans-Kaluhin@users.noreply.github.com>
2022-12-14 07:38:34 +0200

    MODSOURMAN-888 - Link update: Implement API endpoint to retrieve mapping metadata by record type (#714)
    * MODSOURMAN-888 - Link update: Implement API endpoint to retrieve mapping metadata by record type (#710)

--- mod-source-record-manager-server/src/main/java/org/folio/rest/impl/MappingMetadataProviderImpl.java
+++ mod-source-record-manager-server/src/main/java/org/folio/rest/impl/MappingMetadataProviderImpl.java
@@ -5,6 +5,7 @@ import io.vertx.core.Context;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import javax.ws.rs.BadRequestException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.dataimport.util.ExceptionHelper;
@@ -12,6 +13,7 @@ import org.folio.dataimport.util.OkapiConnectionParams;
 import org.folio.rest.jaxrs.resource.MappingMetadata;
 import org.folio.rest.tools.utils.TenantTool;
 import org.folio.services.MappingMetadataService;
+import org.folio.services.util.QueryPathUtil;
 import org.folio.spring.SpringContextUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -45,6 +47,27 @@ public class MappingMetadataProviderImpl implements MappingMetadata {
           .onComplete(asyncResultHandler);
       } catch (Exception e) {
         LOGGER.error("Failed to retrieve MappingMetadataDto entity for JobExecution with id {} for tenant {}", jobExecutionId, tenantId, e);
+        asyncResultHandler.handle(Future.succeededFuture(ExceptionHelper.mapExceptionToResponse(e)));
+      }
+    });
+  }
+
+  @Override
+  public void getMappingMetadataTypeByRecordType(String recordType, Map<String, String> okapiHeaders,
+                                                 Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    vertxContext.runOnContext(v -> {
+      try {
+        OkapiConnectionParams params = new OkapiConnectionParams(okapiHeaders, vertxContext.owner());
+        mappingMetadataService.getMappingMetadataDtoByRecordType(
+            QueryPathUtil.toRecordType(recordType).orElseThrow(() ->
+              new BadRequestException("Only marc-bib, marc-holdings or marc-authority supported")), params)
+          .map(GetMappingMetadataByJobExecutionIdResponse::respond200WithApplicationJson)
+          .map(Response.class::cast)
+          .otherwise(ExceptionHelper::mapExceptionToResponse)
+          .onComplete(asyncResultHandler);
+      } catch (Exception e) {
+        LOGGER.error("Failed to retrieve MappingMetadataDto entity for recordType {} and tenant {}",
+          recordType, tenantId, e);
         asyncResultHandler.handle(Future.succeededFuture(ExceptionHelper.mapExceptionToResponse(e)));
       }
     });
