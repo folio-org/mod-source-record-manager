9836bca903eeafc423e26fc7be4579bcf83eaf84
viacheslav_kolesnyk <Viacheslav_Kolesnyk@epam.com>
2023-05-05 17:37:43 +0200

    - add extensive logging for debug purposes

--- mod-source-record-manager-server/src/main/java/org/folio/services/afterprocessing/FieldModificationServiceImpl.java
+++ mod-source-record-manager-server/src/main/java/org/folio/services/afterprocessing/FieldModificationServiceImpl.java
@@ -5,13 +5,16 @@ import static org.folio.services.afterprocessing.AdditionalFieldsUtil.modifyData
 import io.vertx.core.Future;
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.extern.log4j.Log4j2;
 import org.folio.LinkingRuleDto;
 import org.folio.dataimport.util.OkapiConnectionParams;
 import org.folio.rest.jaxrs.model.Record;
 import org.folio.services.mappers.processor.MappingParametersProvider;
 import org.marc4j.marc.DataField;
+import org.marc4j.marc.Subfield;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @Service
 public class FieldModificationServiceImpl implements FieldModificationService {
   
@@ -25,10 +28,13 @@ public class FieldModificationServiceImpl implements FieldModificationService {
 
   @Override
   public Future<List<Record>> remove9Subfields(String jobExecutionId, List<Record> folioRecords, OkapiConnectionParams okapiParams) {
+    log.info("remove9Subfields:: called for job {}", jobExecutionId);
     return mappingParametersProvider.get(jobExecutionId, okapiParams).map(mappingParameters -> {
+      log.info("remove9Subfields:: mappingParameters retrieved for job {}", jobExecutionId);
       var linkableFields = mappingParameters.getLinkingRules().stream()
         .map(LinkingRuleDto::getBibField)
         .collect(Collectors.toList());
+      log.info("remove9Subfields:: linkableFields extracted for job {}: {}", jobExecutionId, linkableFields);
 
       folioRecords.stream()
         .filter(folioRecord -> Record.RecordType.MARC_BIB.equals(folioRecord.getRecordType()))
@@ -39,9 +45,18 @@ public class FieldModificationServiceImpl implements FieldModificationService {
   }
   
   private void removeSubfields(DataField dataField) {
+    log.info("removeSubfields:: called for dataField {} with subfields {}",
+      dataField.getTag(), dataField.getSubfields().stream()
+        .map(Subfield::getCode)
+        .collect(Collectors.toList()));
     var subfields9 = dataField.getSubfields().stream()
       .filter(subfield -> SUBFIELD_9 == subfield.getCode())
       .collect(Collectors.toList());
     subfields9.forEach(dataField::removeSubfield);
+
+    log.info("removeSubfields:: dataField {} with subfields after removal {}",
+      dataField.getTag(), dataField.getSubfields().stream()
+        .map(Subfield::getCode)
+        .collect(Collectors.toList()));
   }
 }
