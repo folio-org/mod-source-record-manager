d4feb33c8db8ce2268bb08ce08c7be9c65685a94
aliaksandr_fedasiuk <aliaksandr_fedasiuk@epam.com>
2023-05-17 16:42:44 +0500

    MODSOURCE-601: return active connections

--- mod-source-record-manager-server/src/main/java/org/folio/dao/util/PostgresClientFactory.java
+++ mod-source-record-manager-server/src/main/java/org/folio/dao/util/PostgresClientFactory.java
@@ -1,13 +1,25 @@
 
 package org.folio.dao.util;
 
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.pgclient.PgConnection;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.Tuple;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.rest.persist.PostgresClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import static org.folio.rest.persist.PostgresClient.convertToPsqlStandard;
+
 @Component
 public class PostgresClientFactory {
+
+  private static final Logger LOGGER = LogManager.getLogger();
 
   private Vertx vertx;
 
@@ -22,6 +34,25 @@ public class PostgresClientFactory {
    * @return Postgres Client
    */
   public PostgresClient createInstance(String tenantId) {
-    return PostgresClient.getInstance(vertx, tenantId);
+    LOGGER.warn("createInstance:: getPostgresClient");
+    PostgresClient postgresClient = PostgresClient.getInstance(vertx, tenantId);
+    getUsedConnectionNumber(postgresClient).onSuccess(res -> LOGGER.warn("active connection number = {}", res));
+    return postgresClient;
   }
+
+  private Future<Integer> getUsedConnectionNumber(PostgresClient postgresClient) {
+    Promise<RowSet<Row>> promise = Promise.promise();
+    try {
+      postgresClient.execute("select count(state) from pg_stat_activity where state = 'active';", promise);
+    } catch (Exception e) {
+      LOGGER.warn("updateCounterValue:: Failed to get counter value to table", e);
+      promise.fail(e);
+    }
+    return getCounterValueFromRowSet(promise);
+  }
+
+  private Future<Integer> getCounterValueFromRowSet(Promise<RowSet<Row>> promise) {
+    return promise.future().map(resultSet -> resultSet.iterator().next().getInteger(0));
+  }
+
 }
