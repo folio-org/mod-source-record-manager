88eaa58370647f2ffaa78b788c57c7c7b7371623
Kyrylo_Shulzhenko <Kyrylo_Shulzhenko@epam.com>
2023-06-23 16:40:32 +0300

    [MODSOURMAN-995]DI Log: Adjust the logic for the statuses that show in the UI log

--- mod-source-record-manager-server/src/main/resources/templates/db_scripts/create_get_record_processing_log_function.sql
+++ mod-source-record-manager-server/src/main/resources/templates/db_scripts/create_get_record_processing_log_function.sql
@@ -1,48 +1,66 @@
 -- Script to create function to get data import record processing log entries (recordProcessingLogDto).
 DROP FUNCTION IF EXISTS get_record_processing_log(uuid, uuid);
 
+
 CREATE OR REPLACE FUNCTION get_record_processing_log(jobExecutionId uuid, recordId uuid)
-    RETURNS TABLE(job_execution_id uuid, source_id uuid, source_record_order integer, title text, source_record_action_status text, source_entity_error text, instance_action_status text, instance_entity_id text[], instance_entity_hrid text[], instance_entity_error text, holdings_action_status text, holdings_entity_id text[], holdings_entity_hrid text[], holdings_entity_error text, item_action_status text, item_entity_id text[], item_entity_hrid text[], item_entity_error text, authority_action_status text, authority_entity_id text[], authority_entity_error text, po_line_action_status text, po_line_entity_id text[], po_line_entity_hrid text[], po_line_entity_error text, order_entity_id text, invoice_action_status text, invoice_entity_id text[], invoice_entity_hrid text[], invoice_entity_error text, invoice_line_action_status text, invoice_line_entity_id text, invoice_line_entity_hrid text, invoice_line_entity_error text)
+    RETURNS TABLE(job_execution_id uuid, source_id uuid, source_record_order integer, title text,
+				  source_record_action_status text, source_entity_error text, instance_action_status text,
+				  instance_entity_id text[], instance_entity_hrid text[], instance_entity_error text,
+				  holdings_action_status text, holdings_entity_hrid text[], holdings_entity_id text[],
+				   holdings_entity_error text, item_action_status text, item_entity_hrid text[], item_entity_id text[],
+				  item_entity_error text, authority_action_status text, authority_entity_id text[],
+				  authority_entity_error text, po_line_action_status text, po_line_entity_id text[], po_line_entity_hrid text[],
+				  po_line_entity_error text, order_entity_id text, invoice_action_status text, invoice_entity_id text[], invoice_entity_hrid text[], invoice_entity_error text, invoice_line_action_status text, invoice_line_entity_id text, invoice_line_entity_hrid text, invoice_line_entity_error text)
 AS $$
 BEGIN
     RETURN QUERY
-        SELECT records_actions.job_execution_id,
-            records_actions.source_id,
-            records_actions.source_record_order,
-            rec_titles.title,
-            CASE WHEN marc_errors_number != 0 OR marc_actions[array_length(marc_actions, 1)] = 'NON_MATCH'
-                THEN 'DISCARDED'
-              WHEN marc_actions[array_length(marc_actions, 1)] = 'CREATE'
-                THEN 'CREATED'
-              WHEN marc_actions[array_length(marc_actions, 1)] IN ('UPDATE', 'MODIFY')
-                THEN 'UPDATED'
-            END AS source_record_action_status,
-            records_actions.source_entity_error[1],
+        WITH temp_result AS (SELECT id, journal_records.job_execution_id, journal_records.source_id, journal_records.entity_type, journal_records.entity_id, journal_records.entity_hrid,
+    					 CASE WHEN error_max != ''  OR action_type = 'NON_MATCH'
+                    		THEN 'DISCARDED'
+                  		WHEN action_type = 'CREATE'
+                    		THEN 'CREATED'
+                  		WHEN action_type IN ('UPDATE', 'MODIFY')
+                    		THEN 'UPDATED'
+                		END AS action_type, journal_records.action_status, journal_records.action_date, journal_records.source_record_order, journal_records.error, journal_records.instance_id, journal_records.holdings_id, journal_records.order_id, action_type_by_source.title
+    		FROM journal_records
+    		INNER JOIN
+    		(SELECT entity_type as entity_type_max, COALESCE(journal_records.entity_id, journal_records.source_id::TEXT) as entity_id_max,action_status as action_status_max, max(error) AS error_max,(array_agg(id ORDER BY array_position(array['CREATE', 'UPDATE', 'MODIFY', 'NON_MATCH'], action_type)))[1] AS id_max, max(journal_records.title) as title
+            	FROM journal_records
+    			WHERE journal_records.job_execution_id = jobExecutionId AND journal_records.source_id = recordId AND journal_records.entity_type NOT IN ('EDIFACT', 'INVOICE')
+            	GROUP BY entity_type, COALESCE(journal_records.entity_id, journal_records.source_id::TEXT),action_status) AS action_type_by_source
+    		ON journal_records.id = action_type_by_source.id_max)
+        (SELECT
+    	      COALESCE(marc.job_execution_id,instances.job_execution_id,holdings.job_execution_id,items.job_execution_id) AS job_execution_id,
+    	      COALESCE(marc.source_id,instances.source_id,holdings.source_id,items.source_id) AS source_id,
+    	      COALESCE(marc.source_record_order,instances.source_record_order,holdings.source_record_order,items.source_record_order) AS source_record_order,
+    	      COALESCE(marc.title,instances.title,holdings.title,items.title) AS title,
+    	      marc.action_type AS source_record_action_status,
+    	      marc.error AS source_entity_error,
 
-            get_entity_status(instance_actions, instance_errors_number) AS instance_action_status,
-            COALESCE(records_actions.instance_entity_id, records_actions.instance_id_value) AS instance_entity_id,
-            records_actions.instance_entity_hrid,
-            records_actions.instance_entity_error[1],
+    	      instances.action_type AS instance_action_status,
+    	      ARRAY[COALESCE(instances.entity_id,holdings.instance_id,items.instance_id)] AS instance_entity_id,
+    	      ARRAY[instances.entity_hrid] AS instance_entity_hrid,
+    	      instances.error AS instance_entity_error,
 
-            get_entity_status(holdings_actions, holdings_errors_number) AS holdings_action_status,
-            COALESCE(records_actions.holdings_entity_id, records_actions.holdings_id_value) AS holdings_entity_id,
-            records_actions.holdings_entity_hrid,
-            records_actions.holdings_entity_error[1],
+    	      holdings.action_type AS holdings_action_status,
+			      ARRAY[holdings.entity_hrid] AS holdings_entity_hrid,
+    	      ARRAY[COALESCE(holdings.entity_id,items.holdings_id)] AS holdings_entity_id,
+    	      holdings.error AS holdings_entity_error,
 
-            get_entity_status(item_actions, item_errors_number) AS item_action_status,
-            records_actions.item_entity_id,
-            records_actions.item_entity_hrid,
-            records_actions.item_entity_error[1],
+    	      items.action_type AS item_action_status,
+			      ARRAY[items.entity_hrid] AS item_entity_hrid,
+    	      ARRAY[items.entity_id] AS item_entity_id,
+    	      items.error AS item_entity_error,
 
-            get_entity_status(authority_actions, authority_errors_number) AS authority_action_status,
-            records_actions.authority_entity_id,
-            records_actions.authority_entity_error[1],
+    	      authority.action_type AS authority_action_status,
+    	      ARRAY[authority.entity_id] AS authority_entity_id,
+    	      authority.error AS authority_entity_error,
 
-            get_entity_status(po_line_actions, po_line_errors_number) AS po_line_action_status,
-            records_actions.po_line_entity_id,
-            records_actions.po_line_entity_hrid,
-            records_actions.po_line_entity_error[1],
-            records_actions.order_entity_id,
+    	      po_lines.action_type AS po_line_action_status,
+    	      ARRAY[po_lines.entity_id] AS po_line_entity_id,
+    	      ARRAY[po_lines.entity_hrid] AS po_line_entity_hrid,
+    	      po_lines.error AS po_line_entity_error,
+    	      po_lines.order_id AS order_entity_id,
 
             null AS invoice_action_status,
             null AS invoice_entity_id,
@@ -52,62 +70,34 @@ BEGIN
             null AS invoice_line_entity_id,
             null AS invoice_line_entity_hrid,
             null AS invoice_line_entity_error
-        FROM (
-            SELECT journal_records.source_id,
-                journal_records.source_record_order,
-                journal_records.job_execution_id,
-                array_agg(action_type ORDER BY action_date ASC) FILTER (WHERE entity_type = 'MARC_BIBLIOGRAPHIC' OR entity_type = 'MARC_HOLDINGS' OR entity_type = 'MARC_AUTHORITY') AS marc_actions,
-                count(journal_records.source_id) FILTER (WHERE (entity_type = 'MARC_BIBLIOGRAPHIC' OR entity_type = 'MARC_HOLDINGS' OR entity_type = 'MARC_AUTHORITY') AND journal_records.error != '') AS marc_errors_number,
-
-                array_agg(error) FILTER (WHERE entity_type = 'MARC_BIBLIOGRAPHIC' OR entity_type = 'MARC_HOLDINGS' OR entity_type = 'MARC_AUTHORITY') AS source_entity_error,
-
-                (array_agg(action_type ORDER BY action_date DESC) FILTER (WHERE entity_type = 'INSTANCE' AND (entity_id IS NOT NULL OR action_type = 'NON_MATCH')))[1:1] AS instance_actions,
-                count(journal_records.source_id) FILTER (WHERE entity_type = 'INSTANCE' AND journal_records.error != '') AS instance_errors_number,
-
-                (array_agg(entity_hrid ORDER BY action_date DESC) FILTER (WHERE entity_type = 'INSTANCE' AND (entity_id IS NOT NULL OR action_type = 'NON_MATCH')))[1:1] AS instance_entity_hrid,
-                (array_agg(entity_id ORDER BY action_date DESC) FILTER (WHERE entity_type = 'INSTANCE' AND (entity_id IS NOT NULL OR action_type = 'NON_MATCH')))[1:1] AS instance_entity_id,
-                array_agg(error) FILTER (WHERE entity_type = 'INSTANCE') AS instance_entity_error,
-
-                array_agg(action_type) FILTER (WHERE entity_type = 'HOLDINGS') AS holdings_actions,
-                count(journal_records.source_id) FILTER (WHERE entity_type = 'HOLDINGS' AND journal_records.error != '') AS holdings_errors_number,
-
-                array_agg(entity_hrid) FILTER (WHERE entity_type = 'HOLDINGS') AS holdings_entity_hrid,
-                array_agg(entity_id) FILTER (WHERE entity_type = 'HOLDINGS') AS holdings_entity_id,
-                array_agg(error) FILTER (WHERE entity_type = 'HOLDINGS') AS holdings_entity_error,
-
-                array_agg(action_type) FILTER (WHERE entity_type = 'ITEM') AS item_actions,
-                count(journal_records.source_id) FILTER (WHERE entity_type = 'ITEM' AND journal_records.error != '') AS item_errors_number,
-
-                array_agg(entity_hrid) FILTER (WHERE entity_type = 'ITEM') AS item_entity_hrid,
-                array_agg(entity_id) FILTER (WHERE entity_type = 'ITEM') AS item_entity_id,
-                array_agg(error) FILTER (WHERE entity_type = 'ITEM') AS item_entity_error,
-
-                array_agg(action_type) FILTER (WHERE entity_type = 'AUTHORITY') AS authority_actions,
-                count(journal_records.source_id) FILTER (WHERE entity_type = 'AUTHORITY' AND journal_records.error != '') AS authority_errors_number,
-
-                array_agg(entity_id) FILTER (WHERE entity_type = 'AUTHORITY') AS authority_entity_id,
-                array_agg(error) FILTER (WHERE entity_type = 'AUTHORITY') AS authority_entity_error,
-
-                array_agg(action_type) FILTER (WHERE entity_type = 'PO_LINE') AS po_line_actions,
-                count(journal_records.source_id) FILTER (WHERE entity_type = 'PO_LINE' AND journal_records.error != '') AS po_line_errors_number,
-
-                array_agg(entity_hrid) FILTER (WHERE entity_type = 'PO_LINE') AS po_line_entity_hrid,
-                array_agg(entity_id) FILTER (WHERE entity_type = 'PO_LINE') AS po_line_entity_id,
-                array_agg(error) FILTER (WHERE entity_type = 'PO_LINE') AS po_line_entity_error,
-                (array_agg(order_id) FILTER (WHERE entity_type = 'PO_LINE'))[1] AS order_entity_id,
-
-                array_agg(DISTINCT instance_id) FILTER (WHERE instance_id IS NOT NULL AND (entity_type = 'HOLDINGS' OR entity_type = 'ITEM')) AS instance_id_value,
-                array_agg(holdings_id) FILTER (WHERE holdings_id IS NOT NULL AND entity_type = 'ITEM') AS holdings_id_value
-            FROM journal_records
-            WHERE journal_records.job_execution_id = jobExecutionId AND journal_records.source_id = recordId AND journal_records.entity_type NOT IN ('EDIFACT', 'INVOICE')
-            GROUP BY journal_records.source_id, journal_records.source_record_order, journal_records.job_execution_id
-        ) AS records_actions
-        LEFT JOIN (
-            SELECT journal_records.source_id, journal_records.title FROM journal_records
-            WHERE journal_records.job_execution_id = jobExecutionId AND journal_records.source_id = recordId
-        ) AS rec_titles ON rec_titles.source_id = records_actions.source_id AND rec_titles.title IS NOT NULL
-        UNION
-            SELECT invoice_line_info.job_execution_id,
+      FROM
+    	    (SELECT temp_result.job_execution_id, entity_id, temp_result.title, temp_result.source_record_order, action_type, error, temp_result.source_id
+    	    FROM temp_result WHERE entity_type IN ('MARC_BIBLIOGRAPHIC', 'MARC_HOLDINGS', 'MARC_AUTHORITY')) AS marc
+    	LEFT JOIN
+    	    (SELECT action_type, entity_id, temp_result.source_id, entity_hrid, error, temp_result.job_execution_id, temp_result.title, temp_result.source_record_order
+    	    FROM temp_result WHERE entity_type = 'INSTANCE') AS instances
+    	ON marc.source_id = instances.source_id
+    	LEFT JOIN
+    	    (SELECT action_type, entity_id, temp_result.source_id, error, temp_result.job_execution_id, temp_result.title, temp_result.source_record_order
+    	    FROM temp_result WHERE entity_type = 'AUTHORITY') AS authority
+    	ON authority.source_id = marc.source_id
+    	LEFT JOIN
+    	    (SELECT action_type,entity_id,entity_hrid,temp_result.source_id,error,order_id,temp_result.job_execution_id,temp_result.title,temp_result.source_record_order
+    	    FROM temp_result WHERE entity_type = 'PO_LINE') AS po_lines
+    	ON po_lines.source_id = marc.source_id
+      RIGHT JOIN (SELECT NULL AS NULLS) AS NULLS ON true
+    	CROSS JOIN
+    	    (SELECT action_type, entity_id, entity_hrid, error, instance_id, holdings.job_execution_id, holdings.source_id, holdings.title, holdings.source_record_order
+             FROM (SELECT * FROM temp_result WHERE entity_type = 'HOLDINGS') AS holdings RIGHT JOIN (SELECT NULL AS null_holdings) AS null_holdings ON true) AS holdings
+    	CROSS JOIN
+    	    (SELECT action_type, entity_id, holdings_id, entity_hrid, error, instance_id, items.job_execution_id, items.source_id, items.title, items.source_record_order
+             FROM (SELECT * FROM temp_result WHERE entity_type = 'ITEM') AS items RIGHT JOIN (SELECT NULL AS null_item) AS null_item ON true) AS items
+     	WHERE (instances.entity_id = COALESCE(holdings.instance_id, instances.entity_id) OR instances.entity_id IS NULL) AND
+     	(holdings.entity_id = COALESCE(items.holdings_id, holdings.entity_id) OR holdings.entity_id IS NULL) AND
+     	COALESCE(marc.job_execution_id,instances.job_execution_id,holdings.job_execution_id,items.job_execution_id) IS NOT NULL
+    	ORDER BY holdings.entity_hrid)
+      UNION
+        	SELECT invoice_line_info.job_execution_id,
                 invoice_line_info.source_id,
                 records_actions.source_record_order,
                 invoice_line_info.title,
@@ -115,26 +105,32 @@ BEGIN
                   WHEN edifact_actions[array_length(edifact_actions, 1)] = 'CREATE' THEN 'CREATED'
                 END AS source_record_action_status,
                 records_actions.source_record_error[1],
+
                 null AS instance_action_status,
                 null AS instance_entity_id,
                 null AS instance_entity_hrid,
                 null AS instance_entity_error,
+
                 null AS holdings_action_status,
-                null AS holdings_entity_id,
                 null AS holdings_entity_hrid,
+                null AS holdings_entity_id,
                 null AS holdings_entity_error,
+
                 null AS item_action_status,
-                null AS item_entity_id,
                 null AS item_entity_hrid,
+                null AS item_entity_id,
                 null AS item_entity_error,
+
                 null AS authority_action_status,
                 null AS authority_entity_id,
                 null AS authority_entity_error,
+
                 null AS po_line_action_status,
                 null AS po_line_entity_id,
                 null AS po_line_entity_hrid,
                 null AS po_line_entity_error,
                 null AS order_entity_id,
+
                 get_entity_status(records_actions.invoice_actions, records_actions.invoice_errors_number) AS invoice_action_status,
                 records_actions.invoice_entity_id,
                 records_actions.invoice_entity_hrid,
