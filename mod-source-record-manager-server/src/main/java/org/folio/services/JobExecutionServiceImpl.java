26642e7e90bab6df0d1078be9714e6e4facbb61d
Taras_Spashchenko <Taras_Spashchenko@epam.com>
2023-08-08 16:48:25 +0200

    MODDATAIMP-870 JobExecutionServiceImpl create COMPOSITE JobExecutions #5

--- mod-source-record-manager-server/src/main/java/org/folio/services/JobExecutionServiceImpl.java
+++ mod-source-record-manager-server/src/main/java/org/folio/services/JobExecutionServiceImpl.java
@@ -411,10 +411,11 @@ public class JobExecutionServiceImpl implements JobExecutionService {
           JobExecution.SubordinationType.COMPOSITE_PARENT :
           isSingle ?
             JobExecution.SubordinationType.PARENT_SINGLE : JobExecution.SubordinationType.PARENT_MULTIPLE)
-        .withStatus(isSingle ? JobExecution.Status.NEW : JobExecution.Status.PARENT)
-        .withUiStatus(isSingle ?
-          JobExecution.UiStatus.valueOf(Status.valueOf(JobExecution.Status.NEW.value()).getUiStatus())
-          : JobExecution.UiStatus.valueOf(Status.valueOf(JobExecution.Status.PARENT.value()).getUiStatus()));
+        .withStatus(isComposite ? JobExecution.Status.PROCESSING_IN_PROGRESS : isSingle ? JobExecution.Status.NEW : JobExecution.Status.PARENT)
+        .withUiStatus(isComposite ? JobExecution.UiStatus.RUNNING :
+          isSingle ?
+            JobExecution.UiStatus.valueOf(Status.valueOf(JobExecution.Status.NEW.value()).getUiStatus()) :
+            JobExecution.UiStatus.valueOf(Status.valueOf(JobExecution.Status.PARENT.value()).getUiStatus()));
     }
     return job;
   }
