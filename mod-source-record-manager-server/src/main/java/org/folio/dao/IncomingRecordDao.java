30818226b6ae3b7ba6647fa871a715ee8c47d00a
yaroslav-epam <138673581+yaroslav-epam@users.noreply.github.com>
2023-11-07 14:39:22 +0200

    MODSOURMAN-1021: add endpoint to get IncomingRecord by id with DAO and service layers functionality (#823)

--- mod-source-record-manager-server/src/main/java/org/folio/dao/IncomingRecordDao.java
+++ mod-source-record-manager-server/src/main/java/org/folio/dao/IncomingRecordDao.java
@@ -6,11 +6,20 @@ import io.vertx.sqlclient.RowSet;
 import org.folio.rest.jaxrs.model.IncomingRecord;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * DAO interface for the {@link IncomingRecord} entity
  */
 public interface IncomingRecordDao {
+
+  /**
+   * Searches for {@link IncomingRecord} by id
+   *
+   * @param id incomingRecord id
+   * @return optional of incomingRecord
+   */
+  Future<Optional<IncomingRecord>> getById(String id, String tenantId);
 
   /**
    * Saves {@link IncomingRecord} entities into DB
