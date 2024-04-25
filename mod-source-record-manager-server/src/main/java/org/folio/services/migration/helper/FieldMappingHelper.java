f3e2dc607b8751671997c63500ad305603204a29
Viacheslav_Poliakov <Viacheslav_Poliakov@epam.com>
2024-04-25 09:58:04 +0300

    feat(identifier-type): change mapping for 010 sub-fields
    - Clean code
    
    Closes MODSOURMAN-1127

--- mod-source-record-manager-server/src/main/java/org/folio/services/migration/helper/FieldMappingHelper.java
+++ mod-source-record-manager-server/src/main/java/org/folio/services/migration/helper/FieldMappingHelper.java
@@ -21,44 +21,44 @@ public class FieldMappingHelper {
     throw new IllegalStateException("Utility class");
   }
 
+  /**
+   * Create entities array of 010 field of Authority mapping rule
+   *
+   * @return entities JsonArray
+   */
   public static JsonArray get010FieldEntityJsonArray() {
-    return new JsonArray(List.of(
+    return JsonArray.of(
       getEntityJsonObject(IDENTIFIER_TYPE_ID, DESCRIPTION_TYPE_ID + LCCN, SUBFIELD_A),
       getEntityJsonObject(IDENTIFIER_VALUE, DESCRIPTION_VALUE, SUBFIELD_A),
       getEntityJsonObject(IDENTIFIER_TYPE_ID, DESCRIPTION_TYPE_ID + CANCELLED + LCCN, SUBFIELD_Z),
       getEntityJsonObject(IDENTIFIER_VALUE, CANCELLED + DESCRIPTION_VALUE, SUBFIELD_Z)
-    ));
+    );
   }
 
   private static JsonObject getEntityJsonObject(String target, String description, String subfield) {
-    Map<String, Object> map = new LinkedHashMap<>();
-    map.put("target", target);
-    map.put("description", description);
-    map.put("subfield", new JsonArray(List.of(subfield)));
-    map.put("rules", getRules(subfield.equals(SUBFIELD_A), target.equals(IDENTIFIER_TYPE_ID)));
-
-    return new JsonObject(map);
+    return JsonObject.of(
+      "target", target,
+      "description", description,
+      "subfield", JsonArray.of(subfield),
+      "rules", getRules(subfield.equals(SUBFIELD_A), target.equals(IDENTIFIER_TYPE_ID))
+    );
   }
 
   private static JsonArray getRules(boolean isSubfieldA, boolean isTypeIdRules) {
     String prefix = isSubfieldA ? "" : CANCELLED;
-    return new JsonArray(
-      List.of(Map.of("conditions", isTypeIdRules ? getTypeIdConditions(prefix) : getValueConditions()))
-    );
+    return JsonArray.of(JsonObject.of("conditions", isTypeIdRules ? getTypeIdConditions(prefix) : getValueConditions()));
   }
 
   private static JsonArray getTypeIdConditions(String prefix) {
-    var name = new JsonObject(Map.of("name", prefix + LCCN));
-    Map<String, Object> map = new LinkedHashMap<>();
-    map.put("type", "set_identifier_type_id_by_name");
-    map.put("parameter", name);
-
-    return new JsonArray(List.of(new JsonObject(map)));
+    var conditionObject = JsonObject.of(
+      "type", "set_identifier_type_id_by_name",
+      "parameter", JsonObject.of("name", prefix + LCCN)
+    );
+    return JsonArray.of(conditionObject);
   }
 
   private static JsonArray getValueConditions() {
-    var type = new JsonObject(Map.of("type", "trim"));
-
-    return new JsonArray(List.of(type));
+    var type = JsonObject.of("type", "trim");
+    return JsonArray.of(type);
   }
 }
