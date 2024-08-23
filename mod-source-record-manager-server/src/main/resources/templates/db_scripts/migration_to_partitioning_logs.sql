b05d820b3cbffb8c1fcb4dfa5be4d1072c1ea3d2
aliaksandr_fedasiuk <aliaksandr_fedasiuk@epam.com>
2024-08-23 22:41:59 +0500

    MODSOURMAN-1204: fixing names

--- mod-source-record-manager-server/src/main/resources/templates/db_scripts/migration_to_partitioning_logs.sql
+++ mod-source-record-manager-server/src/main/resources/templates/db_scripts/migration_to_partitioning_logs.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS ${myuniversity}_${mymodule}.journal_records_entity_ty
   order_id              text,
   permanent_location_id text,
   tenant_id             text,
-  PRIMARY KEY (id, entity_type)
+  --PRIMARY KEY (id, entity_type)
 ) partition by list (entity_type);
 
 CREATE TABLE IF NOT EXISTS ${myuniversity}_${mymodule}.journal_records_marc_bibliographic PARTITION OF journal_records_entity_type FOR VALUES IN ('MARC_BIBLIOGRAPHIC');
