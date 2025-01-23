748c652d698698fcc08774cf517881e0f8a170c3
Javokhir Abdullaev <101543142+JavokhirAbdullayev@users.noreply.github.com>
2025-01-23 15:11:10 +0500

    Remove polling when RebalanceInProgressException occured during consumption of events  (#957)
    * add retry logic
    
    * completable complete
    
    * some changes
    
    * KAFKA_CONSUMER_AUTO_OFFSET_RESET_CONFIG latest
    
    * remove consumer clean up
    
    * replace polling with timed-waiting
    handle other errors
    
    * change log level
    
    * remove committing offsets if rebalancing error
    
    * fix ms
    
    * add ability to change logging level
    
    * remove unused import

--- mod-source-record-manager-server/src/main/java/org/folio/verticle/DataImportJournalBatchConsumerVerticle.java
+++ mod-source-record-manager-server/src/main/java/org/folio/verticle/DataImportJournalBatchConsumerVerticle.java
@@ -21,7 +21,6 @@ import io.vertx.rxjava3.impl.AsyncResultSingle;
 import io.vertx.rxjava3.kafka.client.consumer.KafkaConsumer;
 import io.vertx.rxjava3.kafka.client.consumer.KafkaConsumerRecord;
 import io.vertx.rxjava3.kafka.client.producer.KafkaHeader;
-import java.time.Duration;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -421,11 +420,12 @@ public class DataImportJournalBatchConsumerVerticle extends AbstractVerticle {
       )
       .onErrorResumeNext(error -> {
         if (error instanceof RebalanceInProgressException) {
-          LOGGER.warn("Rebalance in progress. Retrying offset commit...");
-          return kafkaConsumer.rxPoll(Duration.ofMillis(100))
-            .flatMapCompletable(records -> commitOffset(offsets));
+          LOGGER.warn("Rebalance in progress. Waiting for the re-balance to complete...");
+          return Completable.timer(1, TimeUnit.SECONDS);
         }
-        return Completable.complete();
+        LOGGER.warn("Error committing offsets: {}. Retrying in 1 second...", error.getMessage());
+        return Completable.timer(1, TimeUnit.SECONDS)
+          .andThen(commitOffset(offsets));
       })
       .doOnComplete(() -> LOGGER.info("commitOffset:: Successfully committed offsets: {}", offsets))
       .doOnError(error -> LOGGER.error("commitOffset:: Failed to commit offsets: {}", offsets, error));
