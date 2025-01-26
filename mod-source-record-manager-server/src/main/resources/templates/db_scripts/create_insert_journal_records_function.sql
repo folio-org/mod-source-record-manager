5539c78bddba17deab8a0bd747d411b74b73640f
Javokhir_Abdullaev <javokhir_abdullaev@epam.com>
2025-01-26 20:35:48 +0500

    add function

---
+++ mod-source-record-manager-server/src/main/resources/templates/db_scripts/create_insert_journal_records_function.sql
@@ -0,0 +1,31 @@
+DROP FUNCTION IF EXISTS insert_journal_records(jsonb[]);
+
+CREATE OR REPLACE FUNCTION insert_journal_records(
+    records jsonb[]
+)
+    RETURNS void AS
+$$
+BEGIN
+    PERFORM pg_advisory_xact_lock(12345);
+
+    INSERT INTO journal_records
+    SELECT (r ->> 'id')::uuid,
+           (r ->> 'job_execution_id')::uuid,
+           (r ->> 'source_id')::uuid,
+           r ->> 'entity_type',
+           r ->> 'entity_id',
+           r ->> 'entity_hrid',
+           r ->> 'action_type',
+           r ->> 'action_status',
+           to_timestamp((r ->> 'action_date')::bigint / 1000),
+           (r ->> 'source_record_order')::integer,
+           r ->> 'error',
+           r ->> 'title',
+           r ->> 'instance_id',
+           r ->> 'holdings_id',
+           r ->> 'order_id',
+           r ->> 'permanent_location_id',
+           r ->> 'tenant_id'
+    FROM unnest(records) AS r;
+END;
+$$ LANGUAGE plpgsql;
