0c81e51a20d4e606daabbb049c63a0833a7089a8
Javokhir_Abdullaev <javokhir_abdullaev@epam.com>
2025-01-27 13:30:32 +0500

    clean code

--- mod-source-record-manager-server/src/main/java/org/folio/dao/JournalRecordDao.java
+++ mod-source-record-manager-server/src/main/java/org/folio/dao/JournalRecordDao.java
@@ -1,8 +1,6 @@
 package org.folio.dao;
 
 import io.vertx.core.Future;
-import io.vertx.sqlclient.Row;
-import io.vertx.sqlclient.RowSet;
 import org.folio.rest.jaxrs.model.JobExecutionSummaryDto;
 import org.folio.rest.jaxrs.model.JournalRecord;
 import org.folio.rest.jaxrs.model.RecordProcessingLogDto;
