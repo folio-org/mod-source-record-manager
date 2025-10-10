1947721a8e8110297783967bd6e281c1f4f51e0a
Kateryna_Senchenko <kateryna_senchenko@epam.com>
2025-10-10 14:56:54 +0300

    Add logging of the error

--- mod-source-record-manager-server/src/main/java/org/folio/services/migration/impl/MarcBibMapping338FormatCustomMigration.java
+++ mod-source-record-manager-server/src/main/java/org/folio/services/migration/impl/MarcBibMapping338FormatCustomMigration.java
@@ -9,7 +9,6 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.folio.Record;
 import org.folio.services.MappingRuleService;
 import org.springframework.stereotype.Component;
 
@@ -20,13 +19,13 @@ import java.util.UUID;
 public class MarcBibMapping338FormatCustomMigration extends BaseMappingRulesMigration {
 
   private static final Logger LOGGER = LogManager.getLogger();
+  private static final ObjectMapper MAPPER = new ObjectMapper();
   private static final int ORDER = 14;
   private static final UUID MIGRATION_ID = UUID.fromString("a65b7666-bba8-4c5b-bad3-0410015250cd");
   private static final String DESCRIPTION = "MARC Bib mapping rules: update rule for formatId in 338 field";
   private static final String UNMODIFIED_RULE = "migration/marc_bib/338/unmodified.json";
   private static final String UPDATED_RULE = "migration/marc_bib/338/updated.json";
   private static final String RULE_TAG = "338";
-  private final ObjectMapper mapper = new ObjectMapper();
 
   protected MarcBibMapping338FormatCustomMigration(MappingRuleService mappingRuleService) {
     super(MARC_BIB, ORDER, DESCRIPTION, MIGRATION_ID, mappingRuleService);
@@ -39,8 +38,8 @@ public class MarcBibMapping338FormatCustomMigration extends BaseMappingRulesMigr
       if (ruleArray == null) {
         return rules.encode();
       }
-      JsonNode currentRule = mapper.readTree(ruleArray.encode());
-      JsonNode unmodifiedRule = mapper.readTree(Resources.toByteArray(Resources.getResource(UNMODIFIED_RULE)));
+      JsonNode currentRule = MAPPER.readTree(ruleArray.encode());
+      JsonNode unmodifiedRule = MAPPER.readTree(Resources.toByteArray(Resources.getResource(UNMODIFIED_RULE)));
       if (currentRule.equals(unmodifiedRule)) {
         JsonArray updatedRule = new JsonArray(Resources.toString(Resources.getResource(UPDATED_RULE), StandardCharsets.UTF_8));
         rules.put(RULE_TAG, updatedRule);
@@ -49,7 +48,7 @@ public class MarcBibMapping338FormatCustomMigration extends BaseMappingRulesMigr
         LOGGER.warn("338 rule is customized and was not updated");
       }
     } catch (Exception e) {
-      LOGGER.warn("Cannot update 338 rule in marc-bib mapping rules due to {}", e.getMessage());
+      LOGGER.warn("Cannot update 338 rule in marc-bib mapping rules", e);
     }
 
     return rules.encode();
