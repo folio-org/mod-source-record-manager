ffd3c161ea41b118ac0746024e60b8b313961fe6
Ruslan Lavrov <ruslan_lavrov@epam.com>
2026-06-23 11:51:10 +0300

    Fixed data processing for multiple holdings and items to preserve the order of log entries as they were returned by DB.

--- mod-source-record-manager-server/src/main/java/org/folio/dao/JournalRecordDaoImpl.java
+++ mod-source-record-manager-server/src/main/java/org/folio/dao/JournalRecordDaoImpl.java
@@ -41,6 +41,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -599,6 +600,7 @@ public class JournalRecordDaoImpl implements JournalRecordDao {
       entries.stream()
         .collect(Collectors.groupingBy(
           RecordProcessingLogDto::getIncomingRecordId,
+          LinkedHashMap::new,
           Collectors.mapping(RecordProcessingLogDto::getRelatedHoldingsInfo,
             Collectors.flatMapping(List::stream, toList())
           )));
@@ -607,6 +609,7 @@ public class JournalRecordDaoImpl implements JournalRecordDao {
       entries.stream()
         .collect(Collectors.groupingBy(
           RecordProcessingLogDto::getIncomingRecordId,
+          LinkedHashMap::new,
           Collectors.mapping(RecordProcessingLogDto::getRelatedItemInfo,
             Collectors.flatMapping(List::stream, toList())
           )));
